092b578d5739ff778cecd2e5adf81d83155607bf
mfin0008 <mfin0008@student.monash.edu>
2023-04-17 10:54:02 +1000

    Updated logo and title of page

--- balance/src/App.js
+++ balance/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BALANCE<br/>Team forming dashboard
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
