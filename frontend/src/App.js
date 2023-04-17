39f8a99701c5b0403f4972ff70da00a39d75b984
AbBaSaMo <s.a.baaset.moslih@gmail.com>
2023-04-18 04:26:38 +1000

    refactor: wrapped root react component in ChakraProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import Home from './pages/Home'
 import logo from './assets/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ChakraProvider>
+
   );
 }
 
