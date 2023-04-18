b45972a50b67d3d5f3e886419e2d57e0975a8f38
mfin0008 <mfin0008@student.monash.edu>
2023-04-18 18:54:57 +1000

    fix(NavBar): set color to match wireframes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import Home from './pages/Home'
 import DisplayUnitGroups from './pages/DisplayUnitGroups'
 import logo from './assets/logo.png';
 
+
+const theme = extendTheme({
+  colors: {
+      header_color: {
+        500: "#F0EDE7"
+    },
+  }
+}) 
+
+
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <div className="App">
         <BrowserRouter>
           <Routes>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import { Center, Heading} from "@chakra-ui/react";
 
+
+
 export default function NavBar() {
 
     return (
-        <Center m="20px">
+        <Center p="20px" bgColor="header_color.500">
             <Heading>BaLance: Team Forming Dashboard</Heading>
         </Center>
     
