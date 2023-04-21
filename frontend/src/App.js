039faca8b0b2862d6049a46d53d72449a70202d7
Alex <Kanellis>
2023-04-21 17:03:42 +1000

    feat: allow teachers to upload csv
    BREAKING CHANGE: changed App.js to load the page we are working on
    This should be changed back before pushing to main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import Home from './pages/Home'
+import ImportClass from './pages/ImportClass'
 import logo from './assets/logo.png';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<ImportClass />}/>
           </Routes>
         </BrowserRouter>
       </div>
---
+++ frontend/src/pages/ImportClass.js
@@ -0,0 +1,16 @@
+import logo from "../assets/logo.png";
+
+function ImportPage() {
+    return (
+        <div>
+            <h1>This page is used to:</h1>
+            <h3>- Upload a csv file with a class</h3>
+            <h3>- Upload the students and class info</h3>
+            <h3>- Sort the students into random groups which can be viewed</h3>
+
+        </div> 
+
+    )
+}
+
+export default ImportPage
