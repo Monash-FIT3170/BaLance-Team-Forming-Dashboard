b64f3493340772293a3f1524bb3bc52eef37b6cf
userr1010 <nath0002@student.monash.edu>
2023-04-24 02:03:28 +1000

    UnitHomePage created

---
+++ frontend/src/pages/test.py
@@ -0,0 +1,104 @@
+import React from "react";
+// Chakra imports
+import {
+  Flex,
+  Button,
+  Icon,
+  Image,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { Center, Heading } from "@chakra-ui/react"
+import { IoEllipsisHorizontalSharp } from "react-icons/io5";
+import NavBar from "../components/NavBar.jsx"
+
+function UnitPage() {
+  let boxBg = useColorModeValue("white !important", "#111c44 !important");
+  let mainText = useColorModeValue("gray.800", "white");
+  let secondaryText = useColorModeValue("gray.600", "gray.600");
+  let iconBox = useColorModeValue("gray.100", "whiteAlpha.200");
+  let iconColor = useColorModeValue("brand.200", "white");
+
+  return(
+    <div>
+      <NavBar />
+        <Center margin="40px">
+          <Heading>Unit Home Page</Heading>
+          
+          {/* <Button
+              w='30px'
+              h='40px'
+              alignContent='right'
+              borderRadius='12px'
+              me='12px'>
+                <Icon w='24px' h='24px'as={AddIcon} color={iconColor}/>
+            </Button> */}
+          
+          <br />
+        </Center>
+
+        <Flex 
+        borderRadius='20px'
+        borderColor={'purple'}
+        bg={boxBg}
+        p='20px'
+        h='345px'
+        w={{ base: "315px", md: "345px" }}
+        alignItems='center'
+        direction='column'>  
+        <Flex w='100%' mb='18px'>
+          <Flex
+            w='20px'
+            h='40px'
+            align='left'
+            justify='left'
+            borderRadius='20%'
+            borderColor='black'
+            me='12px'>
+          </Flex>
+            <Text
+              my='auto'
+              fontWeight='800'
+                
+              color={mainText}
+              textAlign='left'
+              fontSize='xl'
+              me='auto'>
+                <Button style={{fontWeight: 'bold', fontSize: '20px'}}>FIT3107</Button>
+              
+            </Text>
+            
+            <Button
+              w='38px'
+              h='38px'
+              align='right'
+              justify='right'
+              borderRadius='12px'
+              me='12px'
+              bg={iconBox}>
+                <Icon
+                w='24px'
+                h ='24px'
+                as={IoEllipsisHorizontalSharp}
+                color={iconColor}/>
+            </Button>
+        </Flex>
+        <Image
+        src='https://img.freepik.com/free-vector/gradient-purple-color-gradient-background-abstract-modern_343694-2243.jpg?w=740&t=st=1682246391~exp=1682246991~hmac=24a5e0adc73d36b09e5b9fc4b2b05aabd12bab82078f67b6556cb3800ca6d1e4'
+        style={{ height: '30%', width: '80%', display: 'flex', justifyContent: 'center'}}
+        borderRadius='20px'
+        mb='10px'/>
+        <Text
+        fontWeight='600'
+        color={secondaryText}
+        textAlign='center'
+        fontSize='l'
+        w='80%'>
+        Software Engineering Practice
+      </Text>
+      </Flex>
+    </div>
+  )
+}
+export default UnitPage;
