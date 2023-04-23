f416744fb9057aedac972b087da97724b8345a46
jhun0012 <jhun0012@student.monash.edu>
2023-04-23 16:21:51 +1000

    Delete Unit Route Created

--- backend/controllers/unitController.js
+++ backend/controllers/unitController.js
@@ -1,1 +1,14 @@
 // TODO controllers to handle requests for unit data
+
+module.exports = {
+
+    deleteUnit: async function (req, res){
+
+        let unitId = req.params.unitId;
+
+        res.send(`${unitId} has been deleted`);
+
+    }
+
+
+}
