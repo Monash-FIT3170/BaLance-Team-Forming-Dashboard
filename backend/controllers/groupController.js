1c43f63703a9319e476e39c3a0e4a4b035a497be
jhun0012 <jhun0012@student.monash.edu>
2023-04-23 16:33:08 +1000

    Get All Groups from Unit Route

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -1,1 +1,16 @@
 // TODO controllers to handle requests for groups data
+
+// TODO controllers to handle requests for unit data
+
+module.exports = {
+
+    getAllGroups: async function (req, res){
+
+        let unitId = req.params.unitId;
+
+        res.send(`Groups from ${unitId}`);
+
+    }
+
+
+}
--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const groupController = require('../controllers/groupController');
+
 // TODO API endpoints for units
 // GET api/units/:id/groups --> all groups in the unit
 
@@ -9,6 +11,8 @@ const router = express.Router()
 router.get('/', (req, res) => {
     console.log("GET all teams");
 })
+
+router.get('/:unitId/groups', groupController.getAllGroups)
 
 // export this router for external use
 module.exports = router
