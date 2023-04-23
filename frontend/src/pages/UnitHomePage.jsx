18c894a566277b2b2eecbe54ec47e506f33a8cf5
userr1010 <nath0002@student.monash.edu>
2023-04-24 02:32:55 +1000

    nav bar added with div

--- frontend/src/pages/UnitHomePage.jsx
+++ frontend/src/pages/UnitHomePage.jsx
@@ -19,4 +19,24 @@ function UnitPage(){
     let secondaryText = useColorModeValue("gray.600", "gray.600");
     let iconBox = useColorModeValue("gray.100", "whiteAlpha.200");
     let iconColor = useColorModeValue("brand.200", "white");
+    
+  return(
+    <div>
+      <NavBar />
+        <Center margin="40px">
+          <Heading>Unit Home Page</Heading>
+          
+          {/* <Button
+              w='30px'
+              h='40px'
+              alignContent='right'
+              borderRadius='12px'
+              me='12px'>
+                <Icon w='24px' h='24px'as={AddIcon} color={iconColor}/>
+            </Button> */}
+          
+          <br />
+        </Center>
+        </div>
+  );
 }export default UnitPage;
