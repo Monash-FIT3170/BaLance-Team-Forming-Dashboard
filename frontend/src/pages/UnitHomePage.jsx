f45927b1f8dbea29be8b58f63f8a8de1b6fc41f0
userr1010 <nath0002@student.monash.edu>
2023-04-24 02:15:44 +1000

    setting variables for cards

--- frontend/src/pages/UnitHomePage.jsx
+++ frontend/src/pages/UnitHomePage.jsx
@@ -14,6 +14,10 @@ import { IoEllipsisHorizontalSharp } from "react-icons/io5";
 import NavBar from "../components/NavBar.jsx"
 
 function UnitPage() {
-    
+    let boxBg = useColorModeValue("white !important", "#111c44 !important");
+    let mainText = useColorModeValue("gray.800", "white");
+    let secondaryText = useColorModeValue("gray.600", "gray.600");
+    let iconBox = useColorModeValue("gray.100", "whiteAlpha.200");
+    let iconColor = useColorModeValue("brand.200", "white");
 }
 export default UnitPage;
