d641a65ea92056857893fbc955691c605a61348c
userr1010 <nath0002@student.monash.edu>
2023-04-24 02:13:02 +1000

    imports from chakra

--- frontend/src/pages/UnitHomePage.jsx
+++ frontend/src/pages/UnitHomePage.jsx
@@ -1,1 +1,14 @@
-//test
+import React from "react";
+// Chakra imports
+import {
+  Flex,
+  Button,
+  Icon,
+  Image,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { Center, Heading } from "@chakra-ui/react"
+import { IoEllipsisHorizontalSharp } from "react-icons/io5";
+import NavBar from "../components/NavBar.jsx"
