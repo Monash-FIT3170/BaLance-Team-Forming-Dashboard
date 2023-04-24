26c26a5bebe730cf5fdd906c2ccc002c37f43982
Alex <Kanellis>
2023-04-24 19:07:48 +1000

    refactor: modified components to be chakra

--- frontend/src/pages/ImportClass.js
+++ frontend/src/pages/ImportClass.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.png";
+import { Box, Heading, Text, Flex, Input } from "@chakra-ui/react";
 
 function ImportPage() {
   const [csvFile, setCsvFile] = useState(null);
-  
+
   const handleFileUpload = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -12,11 +13,10 @@ function ImportPage() {
       reader.onload = (event) => {
         const csvString = event.target.result;
         setCsvFile(file);
-        console.log(csvString)
+        console.log(csvString);
       };
     }
   };
-  
 
   const handleDragOver = (e) => {
     e.preventDefault();
@@ -33,32 +33,26 @@ function ImportPage() {
   };
 
   return (
-    <div
+    <Flex
+      height="100%"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
       onDragOver={handleDragOver}
       onDrop={handleDrop}
-      style={{
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
     >
-      <h1>This page is used to:</h1>
-      <h3>- Upload a csv file with a class</h3>
-      <h3>- Upload the students and class info</h3>
-      <h3>- Sort the students into random groups which can be viewed</h3>
+      <Heading as="h1">This page is used to:</Heading>
       {csvFile ? (
-        <div>
-          <p>Selected CSV file: {csvFile.name}</p>
-        </div>
+        <Box>
+          <Text>Selected CSV file: {csvFile.name}</Text>
+        </Box>
       ) : (
-        <div>
-          <p>Drag and drop a CSV file here, or click to select a file</p>
-          <input type="file" onChange={handleFileUpload} />
-        </div>
+        <Box>
+          <Text>Drag and drop a CSV file here, or click to select a file</Text>
+          <Input type="file" onChange={handleFileUpload} />
+        </Box>
       )}
-    </div>
+    </Flex>
   );
 }
 
