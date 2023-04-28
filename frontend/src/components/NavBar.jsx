dafd49bb61be656b75d8bbcc59dabb20cc348ca5
mfin0008 <mfin0008@student.monash.edu>
2023-04-28 10:52:40 +1000

    added logo to navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,14 @@
-import { Center, Heading} from "@chakra-ui/react";
+import { Image, Container } from "@chakra-ui/react";
+import logo from '../assets/logo_separated.png';
 
 
 
 export default function NavBar() {
 
     return (
-        <Center p="20px" bgColor="balance.header_color">
-            <Heading>BaLance: Team Forming Dashboard</Heading>
-        </Center>
-    
+        <Container p="5" maxH="15vh" minW="100vw" bgColor="balance.header_color" centerContent>
+            <button><Image height="10vh" src={logo} alt="BaLance: Team Forming Dashboard" /></button>
+        </Container>
     )
 
 }
