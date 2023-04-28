351b68c6cecf2e1654ff03769ae87d1911428f66
clau <clau0016@student.monash.edu>
2023-04-28 14:56:10 +1000

    feat:implement GroupCard skeleton

--- frontend/src/components/GroupCard.jsx
+++ frontend/src/components/GroupCard.jsx
@@ -7,3 +7,72 @@ Group card needs to:
     - the student's details (found from the group data) need to be passed into the StudentRow component via props
         - e.g. <Student firstName=_firstName email=_email classNum=_classNum wamAverage=_wamAverage enrolmentStatus=_enrolmentStatus discPersonality=_discPersonality > 
 */
+
+import { Tr, Td, HStack, Spacer } from "@chakra-ui/react";
+import { DragHandleIcon, CloseIcon } from '@chakra-ui/icons'
+import StudentRow from './StudentRow'
+
+const GroupCard = (props) =>{
+    const {labNumber, GroupId, Students} = props
+
+    return (
+        <Card border="1px" marginX="20px" marginBottom="20px">
+            <CardHeader>
+                <HStack>
+                    <Heading>Lab {labNumber} Group {GroupId}</Heading>
+                    <Spacer />
+                </HStack>
+            </CardHeader>
+
+            <CardBody margin="15px">
+                <TableContainer>
+                    <Table variant='striped'>
+                        <Thead>
+                            <Tr>
+                                <Th></Th>
+                                <Th>Name</Th>
+                                <Th>Email Address</Th>
+                                <Th>Lab Number</Th>
+                                <Th>WAM average</Th>
+                                <Th>Enrolment Status</Th>
+                                <Th>DISC Personality</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {Students.map(student =>(
+                                <StudentRow details = {student} key = {student._id}/>
+                            ))}
+                            <Tr>
+                                <Td colspan="7">
+                                    <Center>
+                                        <button><AddIcon /></button>
+                                    </Center>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </TableContainer>
+            </CardBody>
+        </Card>
+    )
+}
+export default function GroupCard(props) {
+
+    return (
+        <Tr>
+            <Td>
+                <HStack>
+                    <button><CloseIcon /></button>
+                    <Spacer />
+                    <button><DragHandleIcon /></button>
+                </HStack>
+            </Td>
+            <Td>{firstName}</Td>
+            <Td>{email}</Td>
+            <Td>{classNum}</Td>
+            <Td>{wamAverage}</Td>
+            <Td>{enrolmentStatus}</Td>
+            <Td>{discPersonality}</Td>
+        </Tr>
+    )
+}
