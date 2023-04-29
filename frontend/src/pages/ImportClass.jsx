5bbcb8331c1f9fbe47450673412d32c88e1763ee
Mark-Mikhail <mmik0003@student.monash.edu>
2023-04-30 03:53:54 +1000

    feat: modified csv upload box with new font (Monsterrat) and styling

--- frontend/src/pages/ImportClass.jsx
+++ frontend/src/pages/ImportClass.jsx
@@ -1,8 +1,31 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.png";
-import { Box, Heading, Text, Flex, Input, Alert, AlertIcon, AlertTitle, AlertDescription } from "@chakra-ui/react";
+import { 
+  Box,
+  Text,
+  Flex,
+  Input,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  ChakraProvider,
+  extendTheme,
+  AlertDescription,
+  Button
+} from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 
+const customTheme = extendTheme({
+  fonts: {
+    heading: "Montserrat, sans-serif",
+    body: "Montserrat, sans-serif",
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 600,
+    bold: 700,
+  },
+})
 
 function ImportPage() {
   const [csvFile, setCsvFile] = useState(null);
@@ -43,41 +66,72 @@ function ImportPage() {
   };
 
   return (
-    <> 
-    <NavBar/>   
-    <Flex
-      height="100%"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      onDragOver={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-      }}
-      onDrop={handleDrop}
-    >
-      {errorMessage && (
-        <Alert status="error" my={4}>
-          <AlertIcon />
-          <Box flex="1">
-            <AlertTitle>Error</AlertTitle>
-            <AlertDescription>{errorMessage}</AlertDescription>
+    <>
+      <NavBar />
+      <Flex
+        height="100%"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        margin="20px"
+      >
+        <Box
+          width="50%"
+          height="175px"
+          border="2px dashed black"
+          borderRadius="md"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          textAlign="center"
+        >
+          <Text
+            fontFamily="Montserrat, sans-serif"
+            fontWeight="bold"
+            fontSize="2xl"
+            mb={4}
+          >
+            Drag and Drop CSV file here
+          </Text>
+          <Box
+            bg="white"
+            borderRadius="md"
+            width="80%"
+            p={4}
+            mb={4}
+            fontWeight="bold"
+          >
+            {csvFile ? (
+              <Text>No file chosen: {csvFile.name}</Text>
+            ) : (
+              <>
+                <Text mb={2}>No file chosen</Text>
+                <Button variant="solid" colorScheme="teal">
+                  Choose File
+                  <input
+                    type="file"
+                    onChange={handleUpload}
+                    style={{ display: "none" }}
+                  />
+                </Button>
+              </>
+            )}
           </Box>
-        </Alert>
-      )}
-      {csvFile ? (
-        <Box>
-          <Text>Selected CSV file: {csvFile.name}</Text>
+          {errorMessage && (
+            <Alert status="error" mb={4}>
+              <AlertIcon />
+              <Box flex="1">
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>{errorMessage}</AlertDescription>
+              </Box>
+            </Alert>
+          )}
         </Box>
-      ) : (
-        <Box>
-          <Text>Drag and drop a CSV file here, or click to select a file</Text>
-          <Input type="file" onChange={handleUpload} />
-        </Box>
-      )}
-    </Flex>
+      </Flex>
     </>
-  );
+  ); 
 }
 
 export default ImportPage;
