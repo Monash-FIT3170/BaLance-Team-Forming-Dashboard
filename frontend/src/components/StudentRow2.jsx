b362a442311f9c15bd7c52fb5cf0254c768e691c
clau <clau0016@student.monash.edu>
2023-05-06 16:10:21 +1000

    feat:create row component to display student details for each student in a list format

---
+++ frontend/src/components/StudentRow2.jsx
@@ -0,0 +1,30 @@
+import {Table, Tr, Td, HStack, Select} from "@chakra-ui/react";
+import { CloseIcon } from '@chakra-ui/icons'
+
+const StudentRow2=(props)=> {
+    /*HTML component for each student in each group in the 'List Students' View*/
+    // desctructure the content of props
+    const {studentFirstName, studentEmail,labId,groupNumber} = props.props
+
+    return (
+        <Tr>
+            <Td>
+                <HStack>
+                    <button><CloseIcon /></button>
+                </HStack>
+            </Td>
+            <Td>{studentFirstName}</Td>
+            <Td>{studentEmail}</Td>
+            <Td>{labId}</Td>
+            <Td>
+            <Select placeholder={'Group '+groupNumber} bg="white">
+                <option value='option1'>Group 1</option>
+                <option value='option2'>Group 2</option>
+                <option value='option3'>Group 3</option>
+            </Select>
+            </Td>
+        </Tr>
+    )
+}
+
+export default StudentRow2
