a70cbe155b53b59cd461accaa835ec8c4ecffa29
francisanthony17 <fant0003@student.monash.edu>
2023-05-08 06:49:15 +1000

    make lab options in assigning page

--- frontend/src/pages/AssigningPage.jsx
+++ frontend/src/pages/AssigningPage.jsx
@@ -1,4 +1,4 @@
-import React
+import {React, useState}
  from "react";
 
  // Chakra imports
@@ -16,6 +16,7 @@ import {
   ModalHeader,
   ModalFooter,
   ModalCloseButton,
+  Input,
   useDisclosure,Select,
   Card, CardBody, CardHeader, CardFooter, Table,
     Thead,
@@ -25,11 +26,21 @@ import {
     Th,
     Td,
     TableCaption,
-    TableContainer, SimpleGrid, Heading, Center, Spacer, HStack
+    TableContainer, SimpleGrid, Heading, Center, Spacer, HStack,
+    Checkbox, CheckboxGroup,
+    Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Box
 } from "@chakra-ui/react";
 
 import { MdFilterList  } from 'react-icons/md'
-import { DragHandleIcon, CloseIcon, AddIcon, ArrowBackIcon } from '@chakra-ui/icons'
+import { DragHandleIcon, CloseIcon, AddIcon, ArrowBackIcon,ChevronDownIcon  } from '@chakra-ui/icons'
 
 import NavBar from "../components/NavBar.jsx"
 
@@ -42,12 +53,36 @@ function AssigningPage(){
     let iconColor = useColorModeValue("brand.200", "white");
     const {isOpen, onOpen, onClose } = useDisclosure()
 
+
+    const [isChecked1, setIsChecked1] = useState(false);
+    const [isChecked2, setIsChecked2] = useState(false);
+    const [checkedValues1, setCheckedValues1] = useState([]);
+    const [checkedValues2, setCheckedValues2] = useState([]);
+    const [checkedValues3, setCheckedValues3] = useState([]);
+    const [checkedValues4, setCheckedValues4] = useState([]);
+    
+    function handleCheckboxChange1(values) {
+        setCheckedValues1(values);
+    }
+    function handleCheckboxChange2(values) {
+        setCheckedValues2(values);
+    }
+    function handleCheckboxChange3(values) {
+        setCheckedValues3(values);
+    }
+    function handleCheckboxChange4(values) {
+        setCheckedValues4(values);
+    }
+
+   
+
+
     return(
         <div>
-            <Spacer></Spacer>
+            {/* <Spacer></Spacer>
             <Button colorScheme='gray' variant='ghost'>
                 <ArrowBackIcon boxSize={7}/>
-            </Button>
+            </Button> */}
             <Center margin="30px">
                 <Heading>Assigning Page</Heading>
             </Center>
@@ -60,13 +95,16 @@ function AssigningPage(){
                             <Button colorScheme='gray' variant='ghost'>
                                 <Icon as={MdFilterList}></Icon>Filter
                             </Button>
+                            
                         </HStack>
+                        <Spacer/>
+                        <Input placeholder='search' />
 
                     </CardHeader>
 
                     <CardBody margin="15px">
                         <TableContainer>
-                            <Table variant='striped'>
+                            <Table variant='striped' size='md'>
                                 <Thead>
                                     <Tr>
                                         <Th></Th>
@@ -88,11 +126,35 @@ function AssigningPage(){
                                         <Td>teacher1</Td>
                                         <Td>aaa0000@student.monash.edu</Td>
                                         <Td>
-                                            <Select placeholder='Select option'>
-                                                <option value='option1'>01</option>
-                                                <option value='option2'>02</option>
-                                                <option value='option3'>03</option>
-                                            </Select>
+                                            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing="10px">
+                                                {checkedValues1.map((card, index) => (
+                                                    <Box key={index} boxShadow="md" rounded="md" p="1">
+                                                    <Text fontSize="s">{card}</Text>
+                                                    </Box>
+                                                ))}
+                                            </SimpleGrid>
+                                                
+                                            <Menu>
+                                                <MenuButton px={4}
+                                                            py={2}
+                                                            transition='all 0.2s'
+                                                            borderRadius='md'
+                                                            borderWidth='1px'
+                                                            _hover={{ bg: 'gray.400' }}
+                                                            _expanded={{ bg: 'blue.400' }}
+                                                            _focus={{ boxShadow: 'outline' }}>Labs <ChevronDownIcon /></MenuButton>
+                                                <MenuList>
+                                                    <MenuOptionGroup
+                                                    type="checkbox"
+                                                    value={checkedValues1}
+                                                    onChange={handleCheckboxChange1}
+                                                    >
+                                                    <MenuItemOption value="lab1">Lab 1</MenuItemOption>
+                                                    <MenuItemOption value="lab2">Lab 2</MenuItemOption>
+                                                    <MenuItemOption value="lab3">Lab 3</MenuItemOption>
+                                                    </MenuOptionGroup>
+                                                </MenuList>
+                                            </Menu>
                                         </Td>
                                         <Td>
                                             <Select placeholder='Select option'>
@@ -113,11 +175,35 @@ function AssigningPage(){
                                         <Td>teacher2</Td>
                                         <Td>aaa0001@student.monash.edu</Td>
                                         <Td>
-                                            <Select placeholder='Select option'>
-                                                <option value='option1'>01</option>
-                                                <option value='option2'>02</option>
-                                                <option value='option3'>03</option>
-                                            </Select>
+                                            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing="10px">
+                                                {checkedValues2.map((card, index) => (
+                                                    <Box key={index} boxShadow="md" rounded="md" p="1">
+                                                    <Text fontSize="s">{card}</Text>
+                                                    </Box>
+                                                ))}
+                                            </SimpleGrid>
+                                                
+                                            <Menu>
+                                                <MenuButton px={4}
+                                                                py={2}
+                                                                transition='all 0.2s'
+                                                                borderRadius='md'
+                                                                borderWidth='1px'
+                                                                _hover={{ bg: 'gray.400' }}
+                                                                _expanded={{ bg: 'blue.400' }}
+                                                                _focus={{ boxShadow: 'outline' }}>Labs <ChevronDownIcon /></MenuButton>
+                                                <MenuList>
+                                                    <MenuOptionGroup
+                                                    type="checkbox"
+                                                    value={checkedValues2}
+                                                    onChange={handleCheckboxChange2}
+                                                    >
+                                                    <MenuItemOption value="lab1">Lab 1</MenuItemOption>
+                                                    <MenuItemOption value="lab2">Lab 2</MenuItemOption>
+                                                    <MenuItemOption value="lab3">Lab 3</MenuItemOption>
+                                                    </MenuOptionGroup>
+                                                </MenuList>
+                                            </Menu>
                                         </Td>
                                         <Td>
                                             <Select placeholder='Select option'>
@@ -138,11 +224,35 @@ function AssigningPage(){
                                         <Td>teacher3</Td>
                                         <Td>aaa0002@student.monash.edu</Td>
                                         <Td>
-                                            <Select placeholder='Select option'>
-                                                <option value='option1'>01</option>
-                                                <option value='option2'>02</option>
-                                                <option value='option3'>03</option>
-                                            </Select>
+                                            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing="10px">
+                                                {checkedValues3.map((card, index) => (
+                                                    <Box key={index} boxShadow="md" rounded="md" p="1">
+                                                    <Text fontSize="s">{card}</Text>
+                                                    </Box>
+                                                ))}
+                                            </SimpleGrid>
+                                                
+                                            <Menu>
+                                                <MenuButton px={4}
+                                                            py={2}
+                                                            transition='all 0.2s'
+                                                            borderRadius='md'
+                                                            borderWidth='1px'
+                                                            _hover={{ bg: 'gray.400' }}
+                                                            _expanded={{ bg: 'blue.400' }}
+                                                            _focus={{ boxShadow: 'outline' }}>Labs <ChevronDownIcon /></MenuButton>
+                                                <MenuList>
+                                                    <MenuOptionGroup
+                                                    type="checkbox"
+                                                    value={checkedValues3}
+                                                    onChange={handleCheckboxChange3}
+                                                    >
+                                                    <MenuItemOption value="lab1">Lab 1</MenuItemOption>
+                                                    <MenuItemOption value="lab2">Lab 2</MenuItemOption>
+                                                    <MenuItemOption value="lab3">Lab 3</MenuItemOption>
+                                                    </MenuOptionGroup>
+                                                </MenuList>
+                                            </Menu>
                                         </Td>
                                         <Td>
                                             <Select placeholder='Select option'>
@@ -163,11 +273,35 @@ function AssigningPage(){
                                         <Td>teacher4</Td>
                                         <Td>aaa0003@student.monash.edu</Td>
                                         <Td>
-                                            <Select placeholder='Select option'>
-                                                <option value='option1'>01</option>
-                                                <option value='option2'>02</option>
-                                                <option value='option3'>03</option>
-                                            </Select>
+                                            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing="10px">
+                                                {checkedValues4.map((card, index) => (
+                                                    <Box key={index} boxShadow="md" rounded="md" p="1">
+                                                    <Text fontSize="s">{card}</Text>
+                                                    </Box>
+                                                ))}
+                                            </SimpleGrid>
+                                                
+                                            <Menu>
+                                                <MenuButton px={4}
+                                                            py={2}
+                                                            transition='all 0.2s'
+                                                            borderRadius='md'
+                                                            borderWidth='1px'
+                                                            _hover={{ bg: 'gray.400' }}
+                                                            _expanded={{ bg: 'blue.400' }}
+                                                            _focus={{ boxShadow: 'outline' }}>Labs <ChevronDownIcon /></MenuButton>
+                                                <MenuList>
+                                                    <MenuOptionGroup
+                                                    type="checkbox"
+                                                    value={checkedValues4}
+                                                    onChange={handleCheckboxChange4}
+                                                    >
+                                                    <MenuItemOption value="lab1">Lab 1</MenuItemOption>
+                                                    <MenuItemOption value="lab2">Lab 2</MenuItemOption>
+                                                    <MenuItemOption value="lab3">Lab 3</MenuItemOption>
+                                                    </MenuOptionGroup>
+                                                </MenuList>
+                                            </Menu>
                                         </Td>
                                         <Td>
                                             <Select placeholder='Select option'>
