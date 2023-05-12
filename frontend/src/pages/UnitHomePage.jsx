0511131b78cb875dbe751cd8c3e58181bc9960d8
francisanthony17 <fant0003@student.monash.edu>
2023-05-12 16:51:43 +1000

    started on homepage functionality, still buggy

--- frontend/src/pages/UnitHomePage.jsx
+++ frontend/src/pages/UnitHomePage.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import createUnitGroups from './DisplayUnitGroups'
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-
+import UnitCard from "../components/UnitCard"
+import "../pages/UnitHomePage.css"
 
 // Chakra imports
 import {
@@ -24,12 +25,47 @@ import {
   FormLabel,
   Input,
   Select,
-  FormErrorMessage
+  FormErrorMessage,
+  Container
 } from "@chakra-ui/react";
 import { PlusSquareIcon, EditIcon } from "@chakra-ui/icons";
 import { Center, Heading } from "@chakra-ui/react"
 import { IoEllipsisHorizontalSharp,IoTrashOutline } from "react-icons/io5";
 import NavBar from "../components/NavBar.jsx"
+
+const units = []
+const eng1003 = {
+	"unitId": "ENG1003",
+  "unitName":"Engineering Mobile Apps",
+	"unitFaculty": "Engineering",
+	"labs": ["001", "002", "003"],
+	"teachers": ["EvanSmith@teacher.monash.edu", "JaneDoe@teacher.monash.edu"]
+}
+const eng1005 = {
+	"unitId": "ENG1005",
+  "unitName":"Engineering Mathematics",
+	"unitFaculty": "Engineering",
+	"labs": ["001", "002", "003"],
+	"teachers": ["EvanSmith@teacher.monash.edu", "JaneDoe@teacher.monash.edu"]
+}
+const eng1001 = {
+	"unitId": "ENG1001",
+  "unitName":"The civil stuff",
+	"unitFaculty": "Engineering",
+	"labs": ["001", "002", "003"],
+	"teachers": ["EvanSmith@teacher.monash.edu", "JaneDoe@teacher.monash.edu"]
+}
+const eng1002 = {
+	"unitId": "ENG1002",
+  "unitName":"The electrical stuff",
+	"unitFaculty": "Engineering",
+	"labs": ["001", "002", "003"],
+	"teachers": ["EvanSmith@teacher.monash.edu", "JaneDoe@teacher.monash.edu"]
+}
+units.push(eng1003)
+units.push(eng1005)
+units.push(eng1001)
+units.push(eng1002)
 
 
 
@@ -116,98 +152,11 @@ function UnitPage() {
         </Center>
         
         
-        <Flex 
-        borderRadius='20px'
-        bg={boxBg}
-        p='20px'
-        h='345px'
-        w={{ base: "315px", md: "345px" }}
-        alignItems='center'
-        direction='column'>  
-
-        <Flex w='100%' mb='18px'>
-          <Flex
-            w='20px'
-            h='40px'
-            align='left'
-            justify='left'
-            borderRadius='20%'
-            borderColor='black'
-            me='12px'>
-          </Flex>
-            <Text
-              my='auto'
-              fontWeight='800'
-                
-              color={mainText}
-              textAlign='center'
-              fontSize='xl'
-              me='auto'>
-                <Button onClick={handleUnitClick} style={{fontWeight: 'bold', fontSize: '20px'}}>FIT3170</Button>
-            </Text>
-            
-            <Button
-              w='38px'
-              h='38px'
-              align='right'
-              justify='right'
-              borderRadius='12px'
-              me='12px'
-              bg={iconBox}
-              onClick={onOpenDetails}>
-                <Icon
-                w='24px'
-                h ='24px'
-                as={IoEllipsisHorizontalSharp}
-                color={iconColor}
-                />
-              
-            </Button>
-            <Modal closeOnOverlayClick={false} isOpen={isOpenDetails} onClose={onCloseDetails} onClick={<Link to={this} onClick={isOpenDetails}>'./DisplayUnitGroups'</Link>}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>FIT3170</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody pb={6}>
-                  150 Students enrolled
-                </ModalBody>
-                <ModalFooter>
-                  <Text
-                    my='auto'
-                    fontWeight='800'
-                      
-                    color={mainText}
-                    textAlign='left'
-                    fontSize='xl'
-                    me='auto'>
-                      <Button> <Icon as={EditIcon}></Icon> </Button>
-
-                    
-                  </Text>
-                  <Button onClick={onCloseDetails} mr={3}>Cancel</Button>
-                  <Button colorScheme='blue' onClick={onCloseDetails}>
-                    Save
-                  </Button>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
-        </Flex>
-        <Button onClick={handleUnitClick} style={{ display: 'inline-block', width: 'auto', height: 'auto' }}  >
-          <Image
-          src='https://img.freepik.com/free-vector/gradient-purple-color-gradient-background-abstract-modern_343694-2243.jpg?w=740&t=st=1682246391~exp=1682246991~hmac=24a5e0adc73d36b09e5b9fc4b2b05aabd12bab82078f67b6556cb3800ca6d1e4'
-          style={{ height: '60%', width: '100%', display: 'flex', justifyContent: 'center'}}
-          borderRadius='20px'
-          mb='10px'/>
-          <Text
-          fontWeight='600'
-          color={secondaryText}
-          textAlign='center'
-          fontSize='l'
-          w='80%'>
-          Software Engineering Practice
-          </Text>
-        </Button>
-      </Flex>
+      <Container className="units" maxW="80vw">
+        {units.map((unit) => (
+          <UnitCard unit={unit} key={unit.unitId} className="unit" />
+        ))}
+      </Container>
     </div>
   )
 }
