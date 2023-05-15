a309ebfc7a6d00ac54df8809db9bbcb4a13fd216
Luke <lbon0008@student.monash.edu>
2023-05-15 12:00:59 +1000

    Student data in the server, correctly formatted

--- frontend/src/pages/students.jsx
+++ frontend/src/pages/students.jsx
@@ -6,70 +6,18 @@ import NavBar from "../components/NavBar";
 import { Link } from "react-router-dom";
 
 function Students() {
-    const data = [{
-        "studentFirstName": "Luke", "studentLastName": "Bonso", "studentEmail": "lbon0008@student.monash.edu",
-        "wam": "HD", "status": "Active", "discPersonality": "DOMINANT", "labId": "1", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "Matt", "studentLastName": "Gan", "studentEmail": "mgan0123@student.monash.edu",
-        "wam": "D", "status": "Active", "discPersonality": "STEADINESS AND INFLUENCE", "labId": "1", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "Steve", "studentLastName": "Madds", "studentEmail": "smad0004@student.monash.edu",
-        "wam": "HD", "status": "Not Active", "discPersonality": "DOMINANT", "labId": "4", "groupNumber": "3"
-    },
-    {
-        "studentFirstName": "Elsa", "studentLastName": "Grain", "studentEmail": "egra0005@student.monash.edu",
-        "wam": "C", "status": "Active", "discPersonality": "DOMINANT", "labId": "4", "groupNumber": "3"
-    },
-    {
-        "studentFirstName": "Jane", "studentLastName": "Smith", "studentEmail": "jsmi0015@student.monash.edu",
-        "wam": "HD", "status": "Not Active", "discPersonality": "DOMINANT", "labId": "4", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "Sarah", "studentLastName": "Low", "studentEmail": "slow0028@student.monash.edu",
-        "wam": "HD", "status": "Active", "discPersonality": "CONSCIENTIOUSNESS", "labId": "1", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "Maria", "studentLastName": "high", "studentEmail": "mhig0048@student.monash.edu",
-        "wam": "C", "status": "Not Active", "discPersonality": "DOMINANT", "labId": "1", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "Matt", "studentLastName": "Higgins", "studentEmail": "mhig0448@student.monash.edu",
-        "wam": "C", "status": "Active", "discPersonality": "STEADINESS AND INFLUENCE", "labId": "1", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "John", "studentLastName": "Doe", "studentEmail": "jdoe0012@student.monash.edu",
-        "wam": "D", "status": "Active", "discPersonality": "CONSCIENTIOUSNESS", "labId": "3", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "John", "studentLastName": "Smith", "studentEmail": "jsmi0006@student.monash.edu",
-        "wam": "HD", "status": "Active", "discPersonality": "CONSCIENTIOUSNESS", "labId": "2", "groupNumber": "3"
-    },
-    {
-        "studentFirstName": "Jim", "studentLastName": "Jones", "studentEmail": "jjon0004@student.monash.edu",
-        "wam": "HD", "status": "Active", "discPersonality": "DOMINANT", "labId": "3", "groupNumber": "2"
-    },
-    {
-        "studentFirstName": "Thomas", "studentLastName": "Main", "studentEmail": "tmai0008@student.monash.edu",
-        "wam": "D", "status": "Active", "discPersonality": "INFLUENCE", "labId": "3", "groupNumber": "3"
-    },
-    {
-        "studentFirstName": "Liz", "studentLastName": "Rey", "studentEmail": "lrey0011@student.monash.edu",
-        "wam": "P", "status": "Active", "discPersonality": "DOMINANT", "labId": "1", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "Connor", "studentLastName": "Loyd", "studentEmail": "cloy0099@student.monash.edu",
-        "wam": "D", "status": "Active", "discPersonality": "INFLUENCE", "labId": "2", "groupNumber": "1"
-    },
-    {
-        "studentFirstName": "James", "studentLastName": "Lonso", "studentEmail": "jlon0028@student.monash.edu",
-        "wam": "D", "status": "Not Active", "discPersonality": "CONSCIENTIOUSNESS", "labId": "1", "groupNumber": "3"
-    },
-    {
-        "studentFirstName": "Anne", "studentLastName": "Stein", "studentEmail": "aste0018@student.monash.edu",
-        "wam": "C", "status": "Active", "discPersonality": "DOMINANT", "labId": "1", "groupNumber": "2"
-    }];
+    
+  const [data, setData] = useState([])
+  const [hasError, setHasError] = useState(false)
+
+
+  useEffect(() => {
+      fetch("http://localhost:8080/api/students/FIT2085_CL_S1_ON-CAMPUS").then(
+          res => res.json().then(
+            res => setData(res)
+          )
+      ).catch(err => setHasError(true))
+  }, [])
 
     return (
         <div>
