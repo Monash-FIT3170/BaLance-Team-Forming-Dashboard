f02efb5ece6786b4047ed5f4b4e31b45ad387f41
clau <clau0016@student.monash.edu>
2023-05-20 19:33:41 +1000

    feat:get student's labId from group api call

--- frontend/src/components/StudentRowStudentDisplay.jsx
+++ frontend/src/components/StudentRowStudentDisplay.jsx
@@ -4,8 +4,21 @@ import ChangeStudentGroupModal from "./ChangeStudentGroupModal";
 const StudentRow2=(props)=> {
     /*HTML component for each student in each group in the 'List Students' View*/
     // desctructure the content of props
-    const {studentFirstName, studentEmail,labId,group} = props.props
+    const {studentId,studentFirstName, studentEmail,group} = props.props
     const { allLabs} = props;
+
+    let labId = 0;
+    console.log(allLabs);
+    for (let i = 0; i < allLabs.length; i++) {
+        for (let j = 0; j < allLabs[i].members.length; j++){
+            console.log(studentId)
+            console.log(allLabs[i].members[j].studentI);
+            console.log("--------------");
+            if (studentId === allLabs[i].members[j].studentId){
+                labId = allLabs[i].labId;
+            }
+        }
+    }
 
     return (
         <Tr>
