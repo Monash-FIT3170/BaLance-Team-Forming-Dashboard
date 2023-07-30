29e57328e0d360d5fd45241e70f12d9d8233776b
jhun0012 <jhun0012@student.monash.edu>
2023-07-31 09:41:40 +1000

    feat: backend authentication implemented

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const unitRoutes = require('./routes/units');
 const groupRoutes = require('./routes/groups');
 const studentRoutes = require('./routes/students');
 const cors = require('cors');
+//const { expressjwt: jwt } = require('express-jwt');
+const { auth } = require('express-oauth2-jwt-bearer');
+const jwks = require('jwks-rsa');
+const axios = require('axios');
 
 // attach .env contents to the global process object
 require('dotenv').config();
@@ -25,6 +29,14 @@ app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
+
+const verifyJwt = auth({
+    audience: 'balance-api-endpoint',
+    issuerBaseURL: 'https://balance.au.auth0.com/',
+    tokenSigningAlg: 'RS256'
+  });
+
+app.use(verifyJwt);
 
 // route middleware
 app.use('/api/units/', unitRoutes);
