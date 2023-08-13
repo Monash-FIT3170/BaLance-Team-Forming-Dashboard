a14cbf472f5445d3337a851cd3b627892a6722fa
jon65 <xinquanyip@gmail.com>
2023-08-14 03:28:56 +1000

    add context state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StudentContextProvider } from './store/student-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StudentContextProvider>
+      <App />
+    </StudentContextProvider>
   </React.StrictMode>
 );
