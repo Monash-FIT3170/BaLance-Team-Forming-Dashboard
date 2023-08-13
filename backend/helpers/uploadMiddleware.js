43b0ed301f8f68d1956352b651a94ef5a3dded8f
jon65 <xinquanyip@gmail.com>
2023-08-14 03:28:29 +1000

    middleware for processing python file upload

---
+++ backend/helpers/uploadMiddleware.js
@@ -0,0 +1,13 @@
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, './tmp/my-uploads')
+    },
+    filename(req, file, cb) {
+      cb(null, `${file.fieldname}-${Date.now()}`)
+    }
+})
+module.exports.upload = multer({ storage });
+
