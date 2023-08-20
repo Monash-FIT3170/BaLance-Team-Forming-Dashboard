d06964322d2e49d46b73e3d0a2c33e497d943a7d
Mark-Mikhail <mmik0003@student.monash.edu>
2023-08-21 05:31:47 +1000

    feat: add UploadPy component to allow for python file uploads

---
+++ frontend/src/components/UploadPy.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Box, Text, Button, HStack, Input } from '@chakra-ui/react';
+import { FiUploadCloud } from 'react-icons/fi';
+
+function UploadPy({ pyFile, handleUpload }) {
+  return (
+    <Box
+      bg={pyFile ? '#00ADB5' : 'white'}
+      borderRadius="md"
+      width="100%"
+      margin="0 3vw 5vw 3vw"
+      fontWeight="bold"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center" // Center the content horizontally
+      alignItems="center" // Center the content vertically
+      border="2px dashed #24265D"
+      color= "black"
+      transition="color 0.3s ease"
+      _hover={{ bg: '#E2E8F0', cursor: 'pointer' }}
+      cursor="pointer"
+    >
+      <HStack>
+        <FiUploadCloud />
+        <Text>
+          {pyFile ? `File: ${pyFile.name}` : 'Submit a .py file below to upload a custom script'}
+        </Text>
+      </HStack>
+      <Input
+        textColor="white"
+        type="file"
+        onChange={(e) => handleUpload(e)}
+        opacity={0}
+        left={0}
+        top={0}
+        cursor="pointer"
+        focusBorderColor='black'
+      />
+    </Box>
+  );
+}
+
+export default UploadPy;
+
