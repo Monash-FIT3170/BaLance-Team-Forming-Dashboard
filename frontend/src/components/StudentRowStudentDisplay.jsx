ed2c8a1730846ac501f7b680e53574b8205af02d
userr1010 <nath0002@student.monash.edu>
2023-08-20 20:54:56 +1000

    fix: delete student enrolment working

--- frontend/src/components/StudentRowStudentDisplay.jsx
+++ frontend/src/components/StudentRowStudentDisplay.jsx
@@ -1,20 +1,44 @@
-import { Tr, Td, HStack } from '@chakra-ui/react';
+import { Tr, Td, useDisclosure} from '@chakra-ui/react';
 import { DeleteIcon } from '@chakra-ui/icons';
+import { useParams } from 'react-router';
 import ChangeStudentGroupModal from './ChangeStudentGroupModal';
 
-const StudentRowStudentDisplay = ({studentData, numberOfGroups, onDelete}) => {
+const StudentRowStudentDisplay = ({ studentData, numberOfGroups, onDelete }) => {
     /* HTML component for each student in each group in the 'List Students' View */
+    const {
+        onClose: onCloseDetails,
+    } = useDisclosure();
+
     const {
         student_id,
         preferred_name,
         last_name,
         email_address,
-        group_number
     } = studentData;
 
-    const handleDelete = () => {
-        onDelete(student_id);
+    const {
+        unitCode,
+        year,
+        period
+    } = useParams();
+
+  const handleDeleteStudentEnrolment = (event) => {
+    event.preventDefault();
+    console.log("delete student");
+    fetch(`http://localhost:8080/api/students/${unitCode}/${year}/${period}/${student_id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    let answer = window.confirm('Student deleted successfully from enrolment');
+    if (answer) {
+        onCloseDetails();
     }
+    window.location.reload();
+  }
 
     return (
         <Tr>
@@ -22,20 +46,12 @@ const StudentRowStudentDisplay = ({studentData, numberOfGroups, onDelete}) => {
             <Td>{preferred_name}</Td>
             <Td>{last_name}</Td>
             <Td>{email_address}</Td>
-            <Td>{group_number}</Td>
-            <Td>
-            <HStack>
-                <ChangeStudentGroupModal
-                    studentData={studentData}
-                    numberOfGroups={numberOfGroups}
-                />
-            </HStack>
-            </Td>
             <Td>
                 <DeleteIcon
-                style={{ cursor: 'pointer'}}
+                    style={{ cursor: 'pointer' }}
+                    onClick={handleDeleteStudentEnrolment}
                 />
-             </Td>
+            </Td>
         </Tr>
     );
 };
