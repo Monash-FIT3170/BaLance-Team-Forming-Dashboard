1e4dae2929934ff85a9310a66201ad4f9a22b9ae
Mark-Mikhail <mmik0003@student.monash.edu>
2023-08-21 05:32:57 +1000

    feat: update uploadGroupScript page to have consistent upload format as

--- frontend/src/pages/UploadGroupScript.jsx
+++ frontend/src/pages/UploadGroupScript.jsx
@@ -4,68 +4,69 @@ import {
   Text,
   Flex,
   IconButton,
-  Input,
   Button,
   Center,
   Alert,
   AlertIcon,
-  HStack,
-  Spacer,
 } from '@chakra-ui/react';
 import { ArrowForwardIcon, ArrowBackIcon } from '@chakra-ui/icons';
-import { Link, useParams, useNavigate } from 'react-router-dom'; // Import the useParams hook
+import { useParams } from 'react-router-dom';
+import PyInfoButton from '../components/PyInfoButton'; // Import your PyInfoButton component
+import UploadPy from '../components/UploadPy'; // Import your UploadPy component
+import { useNavigate } from 'react-router-dom'; // Import the useNavigate hook
 
 function UploadGroupScript() {
-  const [file, setFile] = useState(null);
+  const [pyFile, setPyFile] = useState(null);
   const { unitCode, year, period } = useParams();
-  const navigate = useNavigate(); // Hook to handle navigation
+  const navigate = useNavigate();
 
   const handleUpload = (e) => {
     e.preventDefault();
     const uploadedFile = e.target.files[0];
-    setFile(uploadedFile);
+    setPyFile(uploadedFile);
   };
 
   const handleSubmit = () => {
     // Handle the file submission here, e.g., send it to the server
-    if (file) {
+    if (pyFile) {
       // Make an API call or perform other actions with the uploaded file
-      console.log('File uploaded:', file);
+      console.log('File uploaded:', pyFile);
     } else {
       console.log('No file uploaded.');
     }
   };
 
-  const navigateToGroupCreationDashboard = () => {
-    // Define the navigation logic to the offering dashboard here
+  const navigateToOfferingDashboard = () => {
     navigate(`/createGroups/${unitCode}/${year}/${period}`);
   };
 
   return (
-    <Box minHeight="100vh" padding="4">
-      <Box width="80%" margin="0 auto">
-        <Center>
-          <Button onClick={navigateToGroupCreationDashboard}>
-            <HStack>
-              <ArrowBackIcon />
-              <Spacer />
-              <Text>Return to Group Creation Page</Text>
-            </HStack>
-          </Button>
-        </Center>
-        <Text fontSize="2xl" fontWeight="bold" textAlign="center" mb={4}>
-          Upload Group Script to: {`${unitCode} - ${period} ${year}, **CAMPUS**`}
-        </Text>
-        <Alert status="info" borderRadius="md">
-          <AlertIcon />
-          <Text>
-            Please upload a Python script (.py) that will be used for custom group formation.
-            Make sure the script adheres to the requirements.
+    <Box padding="4">
+      <Button onClick={navigateToOfferingDashboard}>
+        <ArrowBackIcon />
+        Return to Create Groups Page
+      </Button>
+      <Center>
+        <Box width="80%">
+          <Text fontSize="2xl" fontWeight="bold" textAlign="center" mb={4}>
+            Upload Group Script to: {`${unitCode} - ${period} ${year}, **CAMPUS**`}
           </Text>
-        </Alert>
-        <Flex direction="column" alignItems="center" mt={4}>
-          <Input type="file" accept=".py" onChange={handleUpload} />
-          {file && (
+          <Alert status="info" borderRadius="md">
+            <AlertIcon />
+            <Text>
+              Please upload a Python script (.py) that will be used for custom group formation.
+              Make sure the script adheres to the requirements.
+            </Text>
+          </Alert>
+          <PyInfoButton
+            infoHeader=".py file format"
+            infoText="Accepted .py files should contain the necessary script for custom group formation."
+          />
+          <UploadPy
+            pyFile={pyFile}
+            handleUpload={handleUpload}
+          />
+          {pyFile && (
             <Button
               mt={4}
               colorScheme="green"
@@ -75,12 +76,15 @@ function UploadGroupScript() {
               Upload Script
             </Button>
           )}
-        </Flex>
-      </Box>
+        </Box>
+      </Center>
     </Box>
   );
 }
 
 export default UploadGroupScript;
+
+
+
 
 
