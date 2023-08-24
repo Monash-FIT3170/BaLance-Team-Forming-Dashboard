e1f3b49c156fe49495ab03f44db551312e1c2708
Baaset Moslih <s.a.baaset.moslih@gmail.com>
2023-08-24 17:25:20 +1000

    feat: basic start to analytic helper implementation

--- backend/helpers/groupAnalyticsHelpers.js
+++ backend/helpers/groupAnalyticsHelpers.js
@@ -14,6 +14,15 @@ const getUnitAnalyticsBelbin = async (unitCode, year, offering) => {
      *
      */
 
+    const personalityData = {
+        "personality title": "Belbin personality types",
+        "description:": "Based on the work of Meredith Belbin, the Belbin roles broadly " +
+                        "categorised people into one of 3 types: action-oriented, people-oriented " +
+                        "and thinking-oriented.",
+        "data": []
+    }
+
+    // get the proportion of the 3 belbin roles within a unit offering and put it into personalityData.data
 }
 
 const getUnitAnalyticsEffort = async (unitCode, year, offering) => {
@@ -22,6 +31,19 @@ const getUnitAnalyticsEffort = async (unitCode, year, offering) => {
      * of student effort estimations across the offering and returns it
      *
      */
+
+    const belbinData = {
+        "personality title": "Estimated effort",
+        "description:": "Estimated effort is an indicators of how well a student performs and what they are willing " +
+                        " to put into the current unit offering. It is an aggregation of their own estimate of average " +
+                        " assignment marks in the past and number of hours they are willing to put into the unit.",
+        "data": []
+    }
+
+    // get the proportion of different assignment average and hours to put in and append to personalityData.data
+
+
+    // get numerical data and put it in too
 
 }
 
