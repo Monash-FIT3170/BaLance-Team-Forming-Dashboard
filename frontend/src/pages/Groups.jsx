f04e949ba7f1f7b0ba73a65da41f094d5cf08cc2
mfin0008 <mfin0008@student.monash.edu>
2023-08-25 14:01:09 +1000

    feature/ filter students based on class is working (only in groups display)

--- frontend/src/pages/Groups.jsx
+++ frontend/src/pages/Groups.jsx
@@ -2,18 +2,25 @@ import GroupCard from '../components/GroupCard';
 import { useParams } from 'react-router';
 import React, { useState, useEffect } from 'react';
 import {
-  Button,
-  ButtonGroup,
-  HStack,
-  Spacer,
-  Container,
-  Heading,
-  Center,
-  useDisclosure,
-  VStack,
-  Text,
-  Select,
-  Box,
+    Button,
+    ButtonGroup,
+    HStack,
+    Spacer,
+    Container,
+    Heading,
+    Center,
+    useDisclosure,
+    VStack,
+    Text,
+    Select,
+    Box,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
 } from '@chakra-ui/react';
 
 import { Link, useNavigate } from 'react-router-dom';
@@ -21,188 +28,175 @@ import { ShuffleGroups } from '../components/ShuffleGroups';
 import { AddIcon, EditIcon } from '@chakra-ui/icons';
 
 function Groups() {
-  const cancelRef = React.useRef();
-  const navigate = useNavigate();
-  const [groups, setGroups] = useState([]);
+    const cancelRef = React.useRef();
+    const navigate = useNavigate();
+    const [groups, setGroups] = useState([]);
+    const [filteredClass, setFilteredClass] = useState([""]);
 
-  // Confirmation popup for shuffling groups
-  const {
-    isOpen,
-    onOpen,
-    onClose
-  } = useDisclosure();
+    // Confirmation popup for shuffling groups
+    const {
+        isOpen,
+        onOpen,
+        onClose
+    } = useDisclosure();
 
-  // Retrieve route parameters
-  const {
-    groupStrategy,
-    groupSize,
-    variance,
-    unitCode,
-    year,
-    period
-  } = useParams();
+    // Retrieve route parameters
+    const {
+        groupStrategy,
+        groupSize,
+        variance,
+        unitCode,
+        year,
+        period
+    } = useParams();
 
-  const navigateToBelbinUpload = () => {
-    navigate(`/belbinImport/${unitCode}/${year}/${period}`);
-  };
+    const navigateToBelbinUpload = () => {
+        navigate(`/belbinImport/${unitCode}/${year}/${period}`);
+    };
 
-  const navigateToCreateGroups = () => {
-    navigate(`/createGroups/${unitCode}/${year}/${period}`);
-  }
+    const navigateToCreateGroups = () => {
+        navigate(`/createGroups/${unitCode}/${year}/${period}`);
+    }
 
-  const navigateToStudentUploadInfo = () =>{
-    navigate(`/infoImport/${unitCode}/${year}/${period}`);
-  }
+    const navigateToStudentUploadInfo = () => {
+        navigate(`/infoImport/${unitCode}/${year}/${period}`);
+    }
 
-  useEffect(() => {
-    fetch(`http://localhost:8080/api/groups/${unitCode}/${year}/${period}`)
-      .then((res) =>
-        res.json().then(function (res) {
-          console.log(res)
-          setGroups(res);
-        })
-      )
-      .catch((err) => console.error(err));
-  }, []);
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/groups/${unitCode}/${year}/${period}`)
+            .then((res) =>
+                res.json().then(function (res) {
+                    setGroups(res);
+                })
+            )
+            .catch((err) => console.error(err));
+    }, []);
 
-  const handleShuffleGroups = () => {
-    // Close confirmation dialog
-    onClose();
+    let groupsDisplay = groups.length === 0 ?
+        <Box bg='#E6EBF0' w='60vw' p={4} alignContent="center">
+            <Center>
+                No groups have been created for this offering. Click "Create/Reconfigure Groups" to create groups for the offering.
+            </Center>
+        </Box>
+        :
+        <Container className="groups" maxW="80vw">
+            {groups.map((group) => {
+                const cardKey = `${group.lab_number}_${group.group_number}`;
+                console.log(typeof(filteredClass), typeof(group.lab_number))
+                if (filteredClass == "All" | filteredClass == group.lab_number) {
+                    return (<GroupCard groupData={group} numberOfGroups={groups.length} key={cardKey} />);
+                }
+            })}
+        </Container>
 
-    // API call to create groups from scratch - will automatically delete existing groups first
-    // then call createUnitGroups under the hood
-    fetch(`http://localhost:8080/api/groups/shuffle/${unitCode}/${year}/${period}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        groupSize: groupSize,
-        variance: variance,
-        strategy: groupStrategy,
-      })
-    })
-      .then((res) => res.json())
-      .catch((error) => {
-        console.error('Error:', error);
-      })
-      .finally(() => {
-        // Reload the page
-        window.location.reload();
-      });
-  };
+    const classFilterOptions = [{value: "All", label: "All labs"}]
+    const foundClasses = []
+    for (const group of groups) {
+        if (foundClasses.indexOf(group.lab_number) === -1) {
+            foundClasses.push(group.lab_number)
+            classFilterOptions.push({ value: group.lab_number, label: `Lab ${group.lab_number}` })
+        }
+    }
 
-  let groupsDisplay = groups.length === 0 ?
-    <Box bg='#E6EBF0' w='60vw' p={4} alignContent="center">
-      <Center>
-        No groups have been created for this offering. Click "Create/Reconfigure Groups" to create groups for the offering.
-      </Center>
-    </Box>
-    :
-    <Container className="groups" maxW="80vw">
-      {groups.map((group) => {
-        const cardKey = `${group.lab_number}_${group.group_number}`;
-        return (<GroupCard groupData={group} numberOfGroups={17} key={cardKey} />);
-      })}
-    </Container>
+    return (
+        <div>
+            <Heading alignContent={'center'}>
+                <Center margin="10">{unitCode}</Center>
+            </Heading>
 
-  return (
-    <div>
-      <Heading alignContent={'center'}>
-        <Center margin="10">{unitCode}</Center>
-      </Heading>
-
-      <HStack margin="0px 20vw 5vh 20vw" alignContent={'center'}>
-        <VStack>
-          <Button
-            width="18vw"
-            onClick={navigateToStudentUploadInfo}
-            colorScheme="gray"
-            margin-left="20">
-            <HStack>
-              <AddIcon />
-              <Spacer />
-              <Text>Add Students</Text>
-            </HStack>
-          </Button>
-          <Button
-            width="100%"
-            onClick={navigateToBelbinUpload}
-            colorScheme="gray"
-            margin-left="20">
-            <HStack>
-              <AddIcon />
-              <Spacer />
-              <Text>Add Personality Data</Text>
-            </HStack>
-          </Button>
-          <Button
-            width="100%"
-            onClick={navigateToStudentUploadInfo}
-            colorScheme="gray"
-            margin-left="20">
-            <HStack>
-              <AddIcon />
-              <Spacer />
-              <Text>Add Work Ethic Data</Text>
-            </HStack>
-          </Button>
-        </VStack>
+            <HStack margin="0px 20vw 5vh 20vw" alignContent={'center'}>
+                <VStack>
+                    <Button
+                        width="18vw"
+                        onClick={navigateToStudentUploadInfo}
+                        colorScheme="gray"
+                        margin-left="20">
+                        <HStack>
+                            <AddIcon />
+                            <Spacer />
+                            <Text>Add Students</Text>
+                        </HStack>
+                    </Button>
+                    <Button
+                        width="100%"
+                        onClick={navigateToBelbinUpload}
+                        colorScheme="gray"
+                        margin-left="20">
+                        <HStack>
+                            <AddIcon />
+                            <Spacer />
+                            <Text>Add Personality Data</Text>
+                        </HStack>
+                    </Button>
+                    <Button
+                        width="100%"
+                        onClick={navigateToStudentUploadInfo}
+                        colorScheme="gray"
+                        margin-left="20">
+                        <HStack>
+                            <AddIcon />
+                            <Spacer />
+                            <Text>Add Work Ethic Data</Text>
+                        </HStack>
+                    </Button>
+                </VStack>
 
 
-        <Spacer />
-
-        <HStack m="40px">
-          <Spacer />
-          <ButtonGroup colorScheme="#282c34" variant="outline" size="lg" isAttached>
-            <Button isDisabled={true}>  Groups  </Button>
-            <Link to={`/students/${unitCode}/${year}/${period}`}>
-              <Button>
-                Students
-              </Button>
-            </Link>
-          </ButtonGroup>
-          <Spacer />
-        </HStack>
-
-        <Spacer />
-
-        <HStack margin="0px 20vw 5vh 20vw" alignContent={'center'}>
-          <VStack>
-            <Button
-              width="18vw"
-              onClick={navigateToCreateGroups}
-              colorScheme="gray"
-              margin-left="20">
-              <HStack>
-                <EditIcon />
                 <Spacer />
-                <Text>Create/Reconfigure Groups</Text>
-              </HStack>
-            </Button>
-            <Center><Text fontWeight={"semibold"}>Show Students from Class:</Text></Center>
-            <Select
-              placeholder={"All"}
-              value={`unitSemesterOffering`}
-              onChange={(event) => `setUnitSemesterOffering(event.target.value)`}
-            >
-              {`<option value="S1">S1</option>
-                      <option value="S2">S2</option>
-                      <option value="FY">FY</option>`}
-            </Select>
-          </VStack>
-        </HStack>
-      </HStack>
-      <Center>
-        <VStack>
-          {groups.length > 0 && (<Button>Export group data to .csv</Button>)}
-          {groupsDisplay}
-        </VStack>
 
-      </Center>
+                <HStack m="40px">
+                    <Spacer />
+                    <ButtonGroup colorScheme="#282c34" variant="outline" size="lg" isAttached>
+                        <Button isDisabled={true}>  Groups  </Button>
+                        <Link to={`/students/${unitCode}/${year}/${period}`}>
+                            <Button>
+                                Students
+                            </Button>
+                        </Link>
+                    </ButtonGroup>
+                    <Spacer />
+                </HStack>
 
-    </div>
-  );
+                <Spacer />
+
+                <HStack margin="0px 20vw 5vh 20vw" alignContent={'center'}>
+                    <VStack>
+                        <Button
+                            width="18vw"
+                            onClick={navigateToCreateGroups}
+                            colorScheme="gray"
+                            margin-left="20">
+                            <HStack>
+                                <EditIcon />
+                                <Spacer />
+                                <Text>Create/Reconfigure Groups</Text>
+                            </HStack>
+                        </Button>
+                        <Center><Text fontWeight={"semibold"}>Show Students from Class:</Text></Center>
+
+                        <Select
+                            value={filteredClass}
+                            onChange={(event) => setFilteredClass(event.target.value)}
+                        >
+                            {classFilterOptions?.map((option) => (
+                                <option key={option.value} value={option.value}>
+                                    {option.label}
+                                </option>
+                            ))}
+                        </Select>
+                    </VStack>
+                </HStack>
+            </HStack>
+            <Center>
+                <VStack>
+                    {groups.length > 0 && (<Button>Export group data to .csv</Button>)}
+                    {groupsDisplay}
+                </VStack>
+
+            </Center>
+
+        </div>
+    );
 }
 
 export default Groups;
--- frontend/src/pages/Students.jsx
+++ frontend/src/pages/Students.jsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import {
-  Table,
-  Thead,
-  Select,
-  Tr,
-  Th,
-  Tbody,
-  Button,
-  ButtonGroup,
-  HStack,
-  Spacer,
-  Heading,
-  Center,
-  useDisclosure,
-  Text,
-  VStack,
-  Box,
-  Container,
+    Table,
+    Thead,
+    Select,
+    Tr,
+    Th,
+    Tbody,
+    Button,
+    ButtonGroup,
+    HStack,
+    Spacer,
+    Heading,
+    Center,
+    useDisclosure,
+    Text,
+    VStack,
+    Box,
+    Container,
 } from '@chakra-ui/react';
 import StudentRowStudentDisplay from '../components/StudentRowStudentDisplay';
 import { Link, useNavigate } from 'react-router-dom';
@@ -25,203 +25,194 @@ import { ShuffleGroups } from '../components/ShuffleGroups';
 import { AddIcon, EditIcon } from '@chakra-ui/icons';
 
 function Students() {
-  const [students, setStudents] = useState([]);
-  const [numberOfGroups, setNumberOfGroups] = useState(0);
-  const cancelRef = React.useRef();
-  const navigate = useNavigate();
-  const {
-    isOpen,
-    onOpen,
-    onClose
-  } = useDisclosure();
+    const [students, setStudents] = useState([]);
+    const [numberOfGroups, setNumberOfGroups] = useState(0);
+    const cancelRef = React.useRef();
+    const navigate = useNavigate();
+    const {
+        isOpen,
+        onOpen,
+        onClose
+    } = useDisclosure();
 
-  const {
-    groupStrategy,
-    groupSize,
-    variance,
-    unitCode,
-    year,
-    period
-  } = useParams();
+    const {
+        groupStrategy,
+        groupSize,
+        variance,
+        unitCode,
+        year,
+        period
+    } = useParams();
 
-  const navigateToStudentUpload = () => {
-    navigate(`/uploadStudents/${unitCode}/${year}/${period}`);
-  };
+    const navigateToStudentUpload = () => {
+        navigate(`/uploadStudents/${unitCode}/${year}/${period}`);
+    };
 
-  const navigateToWorkEthicUpload = () =>{
-    navigate(`/infoImport/${unitCode}/${year}/${period}`);
-  };
+    const navigateToWorkEthicUpload = () => {
+        navigate(`/infoImport/${unitCode}/${year}/${period}`);
+    };
 
-  const navigateToBelbinUpload = () => {
-    navigate(`/belbinImport/${unitCode}/${year}/${period}`);
-  }
-  
-  const navigateToCreateGroups = () => {
-    navigate(`/createGroups/${unitCode}/${year}/${period}`);
-  }
+    const navigateToBelbinUpload = () => {
+        navigate(`/belbinImport/${unitCode}/${year}/${period}`);
+    }
 
-  useEffect(() => {
-    // fetch students from the backend
-    fetch(`http://localhost:8080/api/students/${unitCode}/${year}/${period}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setStudents(res);
-      })
-      .catch((err) => console.error(err));
+    const navigateToCreateGroups = () => {
+        navigate(`/createGroups/${unitCode}/${year}/${period}`);
+    }
 
-    // fetch groups from the backend
-    fetch(`http://localhost:8080/api/groups/${unitCode}/${year}/${period}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setNumberOfGroups(res.length);
-      })
-      .catch((err) => console.error(err));
+    useEffect(() => {
+        // fetch students from the backend
+        fetch(`http://localhost:8080/api/students/${unitCode}/${year}/${period}`)
+            .then((res) => res.json())
+            .then((res) => {
+                setStudents(res);
+            })
+            .catch((err) => console.error(err));
 
-  }, []);
+        // fetch groups from the backend
+        fetch(`http://localhost:8080/api/groups/${unitCode}/${year}/${period}`)
+            .then((res) => res.json())
+            .then((res) => {
+                setNumberOfGroups(res.length);
+            })
+            .catch((err) => console.error(err));
 
-  const handleShuffleGroups = () => {
-    // Close confirmation dialog
-    onClose();
+    }, []);
 
-    // API call to create groups from scratch - will automatically delete existing groups first
-    fetch(`http://localhost:8080/api/groups/shuffle/${unitCode}/${year}/${period}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        groupSize: groupSize,
-        variance: variance,
-        strategy: groupStrategy,
-      })
-    })
-      .catch((error) => { console.error('Error:', error); })
-      .finally(() => { window.location.reload(); });
-  }
+    const handleShuffleGroups = () => {
+        // Close confirmation dialog
+        onClose();
 
-  let studentsDisplay = students.length === 0 ?
-    <Box bg='#E6EBF0' w='60vw' p={4} alignContent="center">
-      <Center>
-        No students have yet been added to the offering. Click "Add Students" to add students to the offering.
-      </Center>
-    </Box>
-    :
-    <Table variant="striped" width="60vw" size="sm" marginBottom="3vh">
-      <Thead>
-        <Tr>
-          <Th>ID</Th>
-          <Th>Preferred Name</Th>
-          <Th>Last Name</Th>
-          <Th>Email Address</Th>
-        </Tr>
-      </Thead>
+        // API call to create groups from scratch - will automatically delete existing groups first
+        fetch(`http://localhost:8080/api/groups/shuffle/${unitCode}/${year}/${period}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                groupSize: groupSize,
+                variance: variance,
+                strategy: groupStrategy,
+            })
+        })
+            .catch((error) => { console.error('Error:', error); })
+            .finally(() => { window.location.reload(); });
+    }
 
-      <Tbody>
-        {students.map((student) => (
-          <StudentRowStudentDisplay
-            studentData={student}
-            numberOfGroups={numberOfGroups}
-            key={student.student_id}
-          />
-        ))}
-      </Tbody>
-    </Table>
-    ;
+    let studentsDisplay = students.length === 0 ?
+        <Box bg='#E6EBF0' w='60vw' p={4} alignContent="center">
+            <Center>
+                No students have yet been added to the offering. Click "Add Students" to add students to the offering.
+            </Center>
+        </Box>
+        :
+        <Table variant="striped" width="60vw" size="sm" marginBottom="3vh">
+            <Thead>
+                <Tr>
+                    <Th>ID</Th>
+                    <Th>Preferred Name</Th>
+                    <Th>Last Name</Th>
+                    <Th>Email Address</Th>
+                </Tr>
+            </Thead>
 
-  return (
-    <div>
-      <Heading alignContent={'center'}>
-        <Center margin="10">{unitCode}</Center>
-      </Heading>
+            <Tbody>
+                {students.map((student) => (
+                    <StudentRowStudentDisplay
+                        studentData={student}
+                        numberOfGroups={numberOfGroups}
+                        key={student.student_id}
+                    />
+                ))}
+            </Tbody>
+        </Table>
+        ;
 
-      <HStack margin="0px 20vw 5vh 20vw" alignContent={'center'}>
-        <VStack>
-          <Button
-            width="18vw"
-            onClick={navigateToStudentUpload}
-            colorScheme="gray"
-            margin-left="20">
-            <HStack>
-              <AddIcon />
-              <Spacer />
-              <Text>Add Students</Text>
-            </HStack>
-          </Button>
-          <Button
-            width="100%"
-            onClick={navigateToBelbinUpload}
-            colorScheme="gray"
-            margin-left="20"
-            isDisabled={students.length===0}>
-            <HStack>
-              <AddIcon />
-              <Spacer />
-              <Text>Add Personality Data</Text>
-            </HStack>
-          </Button>
-          <Button
-            width="100%"
-            onClick={navigateToWorkEthicUpload}
-            colorScheme="gray"
-            margin-left="20"
-            isDisabled={students.length===0}>
-            <HStack>
-              <AddIcon />
-              <Spacer />
-              <Text>Add Work Ethic Data</Text>
-            </HStack>
-          </Button>
-        </VStack>
+    return (
+        <div>
+            <Heading alignContent={'center'}>
+                <Center margin="10">{unitCode}</Center>
+            </Heading>
+
+            <HStack margin="0px 20vw 5vh 20vw" alignContent={'center'}>
+                <VStack>
+                    <Button
+                        width="18vw"
+                        onClick={navigateToStudentUpload}
+                        colorScheme="gray"
+                        margin-left="20">
+                        <HStack>
+                            <AddIcon />
+                            <Spacer />
+                            <Text>Add Students</Text>
+                        </HStack>
+                    </Button>
+                    <Button
+                        width="100%"
+                        onClick={navigateToBelbinUpload}
+                        colorScheme="gray"
+                        margin-left="20"
+                        isDisabled={students.length === 0}>
+                        <HStack>
+                            <AddIcon />
+                            <Spacer />
+                            <Text>Add Personality Data</Text>
+                        </HStack>
+                    </Button>
+                    <Button
+                        width="100%"
+                        onClick={navigateToWorkEthicUpload}
+                        colorScheme="gray"
+                        margin-left="20"
+                        isDisabled={students.length === 0}>
+                        <HStack>
+                            <AddIcon />
+                            <Spacer />
+                            <Text>Add Work Ethic Data</Text>
+                        </HStack>
+                    </Button>
+                </VStack>
 
 
-        <Spacer />
-
-        <HStack m="40px">
-          <Spacer />
-          <ButtonGroup colorScheme="#282c34" variant="outline" size="lg" isAttached>
-            <Link to={`/groups/${unitCode}/${year}/${period}`}>
-              <Button isDisabled={numberOfGroups===0}>  Groups  </Button>
-            </Link>
-            <Button isDisabled={true}>
-              Students
-            </Button>
-          </ButtonGroup>
-          <Spacer />
-        </HStack>
-
-        <Spacer />
-
-        <HStack margin="0px 20vw 5vh 20vw" alignContent={'center'}>
-          <VStack>
-            <Button
-              width="18vw"
-              onClick={navigateToCreateGroups}
-              colorScheme="gray"
-              margin-left="20"
-              isDisabled={students.length===0}>
-              <HStack>
-                <EditIcon />
                 <Spacer />
-                <Text>Create/Reconfigure Groups</Text>
-              </HStack>
-            </Button>
-            <Center><Text fontWeight={"semibold"}>Show Students from Class:</Text></Center>
-            <Select
-              placeholder={"All"}
-              value={`unitSemesterOffering`}
-              onChange={(event) => `setUnitSemesterOffering(event.target.value)`}
-            >
-              {`<option value="S1">S1</option>
-                      <option value="S2">S2</option>
-                      <option value="FY">FY</option>`}
-            </Select>
-          </VStack>
-        </HStack>
-      </HStack>
 
-      <Center>
-        {studentsDisplay}
-      </Center>
-    </div>
-  );
+                <HStack m="40px">
+                    <Spacer />
+                    <ButtonGroup colorScheme="#282c34" variant="outline" size="lg" isAttached>
+                        <Link to={`/groups/${unitCode}/${year}/${period}`}>
+                            <Button isDisabled={numberOfGroups === 0}>  Groups  </Button>
+                        </Link>
+                        <Button isDisabled={true}>
+                            Students
+                        </Button>
+                    </ButtonGroup>
+                    <Spacer />
+                </HStack>
+
+                <Spacer />
+
+                <HStack margin="0px 20vw 5vh 20vw" alignContent={'center'}>
+                    <VStack>
+                        <Button
+                            width="18vw"
+                            onClick={navigateToCreateGroups}
+                            colorScheme="gray"
+                            margin-left="20"
+                            isDisabled={students.length === 0}>
+                            <HStack>
+                                <EditIcon />
+                                <Spacer />
+                                <Text>Create/Reconfigure Groups</Text>
+                            </HStack>
+                        </Button>
+                        
+                    </VStack>
+                </HStack>
+            </HStack>
+
+            <Center>
+                {studentsDisplay}
+            </Center>
+        </div>
+    );
 }
 
 export default Students;
