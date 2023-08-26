3292d0413c6c716ed05565101676c388a3d4d673
Mark-Mikhail <mmik0003@student.monash.edu>
2023-08-26 19:41:52 +1000

    feat: add custom script preview to upload page

--- frontend/src/pages/UploadGroupScript.jsx
+++ frontend/src/pages/UploadGroupScript.jsx
@@ -16,6 +16,7 @@ import UploadPy from '../components/UploadPy'; // Import your UploadPy component
 import { useNavigate } from 'react-router-dom'; // Import the useNavigate hook
 
 function UploadGroupScript() {
+  const [scriptContent, setScriptContent] = useState('');
   const [pyFile, setPyFile] = useState(null);
   const { unitCode, year, period } = useParams();
   const navigate = useNavigate();
@@ -24,13 +25,40 @@ function UploadGroupScript() {
     e.preventDefault();
     const uploadedFile = e.target.files[0];
     setPyFile(uploadedFile);
+  
+    // Read the content of the file
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setScriptContent(event.target.result);
+    };
+    reader.readAsText(uploadedFile);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async() => {
     // Handle the file submission here, e.g., send it to the server
     if (pyFile) {
-      // Make an API call or perform other actions with the uploaded file
-      console.log('File uploaded:', pyFile);
+      const formData = new FormData();
+      formData.append('pythonFile', pyFile);  // Changed to 'pythonFile' here
+
+      try {
+        const response = await fetch(`http://localhost:8080/api/groups/${unitCode}/${year}/${period}/uploadScript`, {
+          method: 'POST',
+          body: formData,
+          // Note: fetch, by default, detects the form data and sets the correct content type
+        });
+
+        if (response.ok) {
+          console.log('File uploaded successfully.');
+          // Optionally navigate the user to another page or show a success message.
+        } else {
+          console.error('Error uploading the script:', await response.text());
+          // Optionally show an error message to the user.
+        }
+
+      } catch (error) {
+        console.error('Error uploading the script:', error);
+        // Optionally show an error message to the user.
+      }
     } else {
       console.log('No file uploaded.');
     }
@@ -51,21 +79,25 @@ function UploadGroupScript() {
           <Text fontSize="2xl" fontWeight="bold" textAlign="center" mb={4}>
             Upload Group Script to: {`${unitCode} - ${period} ${year}, **CAMPUS**`}
           </Text>
-          <Alert status="info" borderRadius="md">
-            <AlertIcon />
-            <Text>
-              Please upload a Python script (.py) that will be used for custom group formation.
-              Make sure the script adheres to the requirements.
-            </Text>
-          </Alert>
+          <Center>
+            <Alert status="info" borderRadius="md">
+              <AlertIcon />
+              <Text>
+                Please upload a Python script (.py) that will be used for custom group formation.
+                Make sure the script adheres to the requirements.
+              </Text>
+            </Alert>
+          </Center>
           <PyInfoButton
             infoHeader=".py file format"
             infoText="Accepted .py files should contain the necessary script for custom group formation."
           />
-          <UploadPy
-            pyFile={pyFile}
-            handleUpload={handleUpload}
-          />
+          <Center>
+            <UploadPy
+              pyFile={pyFile}
+              handleUpload={handleUpload}
+            />
+          </Center>
           {pyFile && (
             <Button
               mt={4}
@@ -78,6 +110,12 @@ function UploadGroupScript() {
           )}
         </Box>
       </Center>
+      {scriptContent && (
+        <>
+            <h3>Script Preview:</h3>
+            <pre>{scriptContent}</pre>
+        </>
+      )}
     </Box>
   );
 }
