39c75340ab691b9c3b6c1605a4c29b98a2fe35a2
Mark-Mikhail <mmik0003@student.monash.edu>
2023-08-28 06:26:29 +1000

    feat: add storeGroupsInDatabase function to insert groups from cusotm script

--- backend/controllers/scriptController.js
+++ backend/controllers/scriptController.js
@@ -1,78 +1,139 @@
 const multer = require('multer');
 const { spawn } = require('child_process');
-const { promisify } = require('util');
 
-const studentController = require('./studentController');
 const { promiseBasedQuery } = require('../helpers/commonHelpers');
 
-const getStudentData = async (unitCode, year, period) => {
-	console.log('IM HERE');
-	const studentsData = await promiseBasedQuery(
-		'SELECT stud.student_id, stud.preferred_name, stud.last_name, stud.email_address, stud.wam_val ' +
-			'FROM student stud ' +
-			'inner join unit_enrolment ue on ue.stud_unique_id=stud.stud_unique_id ' +
-			'inner join unit_offering unit on ue.unit_off_id=unit.unit_off_id ' +
-			'WHERE unit.unit_code=? ' +
-			'AND unit.unit_off_year=? ' +
-			'AND unit.unit_off_period=?',
+async function storeGroupsInDatabase(unitCode, year, period, parsedOutput) {
+	const unitOffIdResult = await promiseBasedQuery(
+		'SELECT unit_off_id FROM unit_offering WHERE unit_code = ? AND unit_off_year = ? AND unit_off_period = ?',
 		[ unitCode, year, period ]
 	);
-	//console.log(studentsData);
-	return studentsData; // return the student data instead of sending a response
-};
 
-const uploadCustomScript = async (req, res) => {
-	const { unitCode, year, period } = req.params;
-	// Step 1: Load student data for the unit
-	const students = await getStudentData(unitCode, year, period);
-	//console.log(students);
+	if (!unitOffIdResult.length) throw new Error('Invalid unit code, year, or period provided.');
 
-	try {
-		// Ensure that scriptContent is in the request body
-		const { scriptContent } = req.body;
-		if (!scriptContent) {
-			return res.status(400).send('Script content is missing.');
+	const unitOffId = unitOffIdResult[0].unit_off_id;
+
+	for (let labId in parsedOutput) {
+		// Check if the lab exists
+		const labExists = await promiseBasedQuery(
+			'SELECT unit_off_lab_id FROM unit_off_lab WHERE lab_number = ? AND unit_off_id = ?',
+			[ labId, unitOffId ]
+		);
+
+		let currentLabId;
+
+		if (!labExists.length) {
+			const result = await promiseBasedQuery(
+				'INSERT INTO unit_off_lab (unit_off_id, lab_number) VALUES (?, ?);',
+				[ unitOffId, parseInt(labId) ]
+			);
+			currentLabId = result.insertId; // Capture the generated primary key
+		} else {
+			currentLabId = labExists[0].unit_off_lab_id; // Use the existing lab's ID
 		}
 
-		// Step 2: Run the script with the student data as input
-		const pythonArgs = [ unitCode, year, period, JSON.stringify(students) ];
+		const groupsInThisLab = parsedOutput[labId];
+		for (let i = 0; i < groupsInThisLab.length; i++) {
+			const group = groupsInThisLab[i];
+			const groupNumber = i + 1;
+
+			// Check if the combination already exists
+			const labGroupExists = await promiseBasedQuery(
+				'SELECT * FROM lab_group WHERE unit_off_lab_id = ? AND group_number = ?',
+				[ currentLabId, groupNumber ]
+			);
+
+			if (!labGroupExists.length) {
+				// If it doesn't exist, perform the insertion
+				await promiseBasedQuery('INSERT INTO lab_group (unit_off_lab_id, group_number) VALUES (?, ?);', [
+					currentLabId,
+					groupNumber
+				]);
+			} else {
+				console.warn(
+					`Lab Group with unit_off_lab_id: ${currentLabId} and group_number: ${groupNumber} already exists.`
+				);
+			}
+
+			console.log(group);
+			var labGroupId;
+			const result = await promiseBasedQuery('SELECT lab_group_id FROM lab_group WHERE unit_off_lab_id = ?', [
+				currentLabId
+			]);
+			if (result && result.length > 0) {
+				labGroupId = result[0].lab_group_id;
+			} else {
+				throw new Error('No corresponding lab_group_id found for given unit_off_lab_id');
+			}
+
+			for (let student of group.members) {
+				await promiseBasedQuery('INSERT INTO group_allocation (stud_unique_id, lab_group_id) VALUES (?, ?);', [
+					student.stud_unique_id,
+					labGroupId
+				]);
+			}
+		}
+	}
+}
+
+async function getStudentData(unitCode, year, period) {
+	return await promiseBasedQuery(
+		'SELECT stud.stud_unique_id, stud.student_id, stud.preferred_name, stud.last_name, stud.email_address, stud.wam_val ' +
+			'FROM student stud ' +
+			'INNER JOIN unit_enrolment ue ON ue.stud_unique_id = stud.stud_unique_id ' +
+			'INNER JOIN unit_offering unit ON ue.unit_off_id = unit.unit_off_id ' +
+			'WHERE unit.unit_code = ? AND unit.unit_off_year = ? AND unit.unit_off_period = ?',
+		[ unitCode, year, period ]
+	);
+}
+
+async function getStudentLabAllocations(students) {
+	const studentIds = students.map((s) => s.stud_unique_id);
+	return await promiseBasedQuery(
+		'SELECT stud_unique_id, unit_off_lab_id FROM student_lab_allocation WHERE stud_unique_id IN (?)',
+		[ studentIds ]
+	);
+}
+
+async function uploadCustomScript(req, res) {
+	const { unitCode, year, period } = req.params;
+	const students = await getStudentData(unitCode, year, period);
+	console.log('Students:', students);
+
+	const studentLabAllocations = await getStudentLabAllocations(students);
+	console.log('Student Lab Allocations:', studentLabAllocations);
+
+	const groupSize = req.body.groupSize || 5; // assuming a default group size of 5 if not provided
+
+	try {
+		const { scriptContent } = req.body;
+		if (!scriptContent) throw new Error('Script content is missing.');
+
+		// Modified pythonArgs to include the studentLabAllocations and groupSize
+		const pythonArgs = [ JSON.stringify(students), JSON.stringify(studentLabAllocations), groupSize.toString() ];
 		const pythonProcess = spawn('python', [ '-c', scriptContent, ...pythonArgs ]);
 
 		let output = '';
 		await new Promise((resolve, reject) => {
-			pythonProcess.on('close', (code) => {
-				if (code === 0) {
-					resolve();
-				} else {
-					reject(new Error(`Failed to run python script with code: ${code}`));
-				}
-			});
-
 			pythonProcess.stdout.on('data', (data) => {
 				output += data.toString();
 			});
-
 			pythonProcess.stderr.on('error', (data) => {
-				console.error(data.toString());
-				reject(new Error('An error occurred while processing the script.' + data.toString()));
+				reject(new Error('Error in script execution. ' + data.toString()));
+			});
+			pythonProcess.on('close', (code) => {
+				if (code === 0) resolve();
+				else reject(new Error(`Python script exited with code: ${code}`));
 			});
 		});
 
-		console.log(output);
-		// Parse the output to JSON
 		const parsedOutput = JSON.parse(output);
-		console.log(parsedOutput);
-		// Step 3: Read the output groups and send these groups to our database
-		//await storeGroupsInDatabase(unitCode, year, period, parsedOutput);
-
-		// Send a success response
-		//res.json({ message: "Groups generated and stored successfully." });
+		await storeGroupsInDatabase(unitCode, year, period, parsedOutput);
+		res.json({ message: 'Groups generated and stored successfully.' });
 	} catch (error) {
 		console.error('An unexpected error occurred:', error);
-		res.status(500).json({ error: 'An error occurred while processing the request.' });
+		res.status(500).json({ error: error.message || 'An error occurred while processing the request.' });
 	}
-};
+}
 
-module.exports = {
-	uploadCustomScript
-};
+module.exports = { uploadCustomScript };
