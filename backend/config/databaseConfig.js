b6d773de26762c2e86b3b6e36efff8d37378e159
jon65 <xinquanyip@gmail.com>
2023-10-18 14:40:04 +1100

    fix databaseConfig accidental push

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -10,11 +10,13 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "A19003607",
-    database: "student_group_db"
+
+const connection = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    connectionLimit: 10
 });
 
 module.exports = connection;
