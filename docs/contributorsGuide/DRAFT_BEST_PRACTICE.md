1b61bd3b7adf03ea3c9955a8f97c276cc69b0889
Baaset Moslih <s.a.baaset.moslih@gmail.com>
2023-12-27 15:29:26 +1100

    refactor: updated NavBar.jsx to arrow function and 4 space indent, cleaned return statement

---
+++ docs/contributorsGuide/DRAFT_BEST_PRACTICE.md
@@ -0,0 +1,7 @@
+This doc outlines some best practice and/or tips learnt along the way
+
+Usage of useNav vs Link 
+
+* use navigate when a redirect is needed as part of another action e.g. after form submission
+* use link when you want something to link to a page without other actions
+* [source](reddit.com/r/reactjs/comments/vq2hb6/which_is_better_to_use_link_or_usenavigate)
--- frontend/src/components/_shared/NavBar.jsx
+++ frontend/src/components/_shared/NavBar.jsx
@@ -1,65 +1,57 @@
-import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import {
-  Image,
-  Container,
-  Button,
-  Box,
-  HStack
+    Image,
+    Container,
+    Button,
+    Box,
+    HStack
 } from '@chakra-ui/react';
 
 import { MockAuth } from '../../helpers/mockAuth';
 import logo from '../../assets/logo_separated.png';
 
-export default function NavBar({ authenticated }) {
-  const navigate = useNavigate();
+const NavBar = ({ authenticated }) => {
+    let authService = {
+        "DEV": MockAuth,
+        "TEST": useAuth0
+    }
 
-  let authService = {
-    "DEV": MockAuth,
-    "TEST": useAuth0
-  }
-
-  const { loginWithRedirect, logout } = authService[process.env.REACT_APP_AUTH]();
-
-  const navigateToHomePage = () => {
-    navigate('/');
-  };
-
-  return (
-    <Container
-      as="header"
-      pos="fixed"
-      top={0}
-      left={0}
-      w="100%"
-      display="flex"
-      p="5"
-      maxH="12vh"
-      minW="100vw"
-      bgColor="balance.header_color"
-      centerContent
-      overflow="hidden"
-      zIndex={999}
-    >
-      <HStack w="100%">
-        <Box width='40%'>
+    const { loginWithRedirect, logout } = authService[process.env.REACT_APP_AUTH]();
 
 
-        </Box>
-        <button onClick={navigateToHomePage} width="10%">
-        {' '}
-        <Image height="8vh" src={logo} alt="BaLance: Team Forming Dashboard" />
-      </button>
-      <Box width='30%'>
+    return (
+        <Container
+            as="header"
+            pos="fixed"
+            top={0}
+            left={0}
+            w="100%"
+            display="flex"
+            p="5"
+            maxH="10em"
+            minW="100vw"
+            bgColor="balance.header_color"
+            centerContent
+            overflow="hidden"
+            zIndex={999}
+        >
+            <HStack w="100%">
+                <Box width='40%'/>
+                <button onClick={navigateToHomePage} width="10%">
+                    {' '}
+                    <Image height="8vh" src={logo} alt="BaLance: Team Forming Dashboard" />
+                </button>
+                <Box width='30%'/>
 
-      </Box>
-      {!authenticated && (
-        <Button onClick={loginWithRedirect} height="8vh">Login</Button>
-      )}
-      {authenticated && (
-        <Button onClick={logout} height="8vh">Logout</Button>
-      )}
-      </HStack>
-    </Container>
-  );
+                {authenticated ? (
+                    <Button onClick={logout} height="8vh">Logout</Button>
+                ) : (
+                    <Button onClick={loginWithRedirect} height="8vh">Login</Button>
+                )}
+
+            </HStack>
+        </Container>
+    );
 }
+
+export default NavBar;
