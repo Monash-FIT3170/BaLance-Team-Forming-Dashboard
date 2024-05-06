0403af1919ebd1a1954986f0f78ce8ec8f57e74c
Alucardigan <thejas.t.vinod@gmail.com>
2024-05-06 12:46:22 +1000

    [STYLE]: Added better styling of contributor components

--- frontend/src/pages/ContributorsPage.jsx
+++ frontend/src/pages/ContributorsPage.jsx
@@ -1,44 +1,44 @@
 //external
 import { useEffect, useState } from "react"
-import { Avatar, Box, Grid, GridItem, Link, Text, Wrap, WrapItem} from '@chakra-ui/react'
+import { Avatar, Box, Grid, GridItem, Heading, Link, Text, Wrap, WrapItem } from '@chakra-ui/react'
 //internal
 
 /*
 Contributors page: used to display the contributions 
 */
-export default function ContributorsPage(){
+export default function ContributorsPage() {
     //link to the contributions file. Need to confirm that this is best practice
     const CONTRIBUTORS_FILE = "https://raw.githubusercontent.com/Monash-FIT3170/BaLance-Team-Forming-Dashboard/contributors_page/.all-contributorsrc"
     //contributors are a state variable so they are automatically updated when they are changed 
-    const [constributors,setContributors] = useState([])
-    useEffect(()=>{
-        fetch(CONTRIBUTORS_FILE,{
-            method:"GET",
-            mode:"cors",//need this to pass header check
-            
-        }
-        ).then(response => response.json()).then(data=>{//convert response to json then set the state variable to the json 
-            setContributors(data.contributors)
-            
-        },
-    )
-    },[]);//useEffect hook that has no dependency. Used to fetch the allconstributors file on page load and update the contributors state variable 
+    const [constributors, setContributors] = useState([])
+    useEffect(() => {
+        fetch(CONTRIBUTORS_FILE, {
+            method: "GET",
+            mode: "cors",//need this to pass header check
 
-    return(
-        <div className="Contributors-Page">
-            <h1>Contributors Page</h1>
-            <Wrap>
-                {constributors.map((contributor)=> Contributor(contributor))}
-            </Wrap>
-            
-        </div>
+        }
+        ).then(response => response.json()).then(data => {//convert response to json then set the state variable to the json 
+            setContributors(data.contributors)
+
+        },
+        )
+    }, []);//useEffect hook that has no dependency. Used to fetch the allconstributors file on page load and update the contributors state variable 
+
+    return (
+        <Box paddingInline={'2rem'} marginBottom={'4rem'}>
+            <Heading>Contributors Page</Heading>
+            <Grid templateColumns={'repeat(auto-fit, minmax(250px, 1fr))'} gap={6}>
+                {constributors.map((contributor) => Contributor(contributor))}
+            </Grid>
+
+        </Box>
     )
 }
 /*'
 Contributors Component
 Takes a contributor object in the format of the allcontributors guide and fits it into a neat component. 
 */
-export  function Contributor(contributor){
+export function Contributor(contributor) {
     //TODO: Need a better way to do this
     const emojiKey = {
         "audio": "🔊",
@@ -73,19 +73,23 @@ export  function Contributor(contributor){
         "tutorial": "✅",
         "talk": "📢",
         "userTesting": "📓",
-        "video": "📹"        
+        "video": "📹"
     }
     //TODO: Add some better styling
     return (
-        <WrapItem p={4} boxShadow={'md'}>
-            <Box textAlign={"center"}>
-                <Avatar src = {contributor.avatar_url}/>
-                <Link href={contributor.profile} >{contributor.name}</Link>
-            </Box>
-            <Box textAlign={'center'}>
-                {contributor.contributions.map((contributionType)=> emojiKey[contributionType])}
-
-            </Box>
-        </WrapItem>
+        <GridItem p={4} boxShadow={'md'}>
+            <Grid templateRows='repeat(2, 1fr)'templateColumns='repeat(2, 1fr)' justifyItems={'center'}>
+                <GridItem rowSpan={2} colSpan={1} >
+                    <Avatar src={contributor.avatar_url} />
+                </GridItem>
+                <GridItem colSpan={1}>
+                    <Link href={contributor.profile} >{contributor.name}</Link>
+                </GridItem>
+                <GridItem colSpan={1}>
+                    {contributor.contributions.map((contributionType) => emojiKey[contributionType])}
+                </GridItem>
+            </Grid>
+            
+        </GridItem>
     )
 }
