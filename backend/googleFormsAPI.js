91cb76867159df591aab4accffcb2087e4a9cd9c
Alucardigan <thejas.t.vinod@gmail.com>
2024-08-08 15:08:24 +1000

    DEBUG: FIxed error when running requests via service account json

--- backend/googleFormsAPI.js
+++ backend/googleFormsAPI.js
@@ -1,26 +1,29 @@
 const { google } = require('googleapis');
 const { GoogleAuth } = require('google-auth-library');
-const path = require('path');
+require('dotenv').config();``
 
-const SERVICE_ACCOUNT_FILE = JSON.parse(process.env.SERVICE_ACCOUNT_FILE)
+const SERVICE_ACCOUNT_JSON = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_TOKEN )
 const SCOPES = ['https://www.googleapis.com/auth/forms', 'https://www.googleapis.com/auth/drive'];
 const auth = new GoogleAuth({
-    keyFile: SERVICE_ACCOUNT_FILE,
+    credentials: SERVICE_ACCOUNT_JSON,
     scopes: SCOPES,
 });
 
-const authClient = await auth.getClient();
-const forms = google.forms({ version: 'v1', auth: authClient });
 
-async function createForm(service,formBody){
-  const result = await service.forms.create({ requestBody: formBody });
-  return result;
+
+async function createForm(auth,formBody){
+    const authClient = await auth.getClient();
+    const forms = google.forms({ version: 'v1', auth: authClient });
+    const result = await forms.forms.create({ requestBody: formBody });
+    return result;
 }
 //note: this can probably only fetch responses from forms the service account has access to, either send the form to the email or make the account create it using createForm. 
-async function getFormResponses(service,formId){
-    
-  const responses = await service.forms.responses.list({ formId });
-  return responses;
+async function getFormResponses(auth,formId){
+    const authClient = await auth.getClient();
+    const forms = google.forms({ version: 'v1', auth: authClient });
+    const responses = await forms.forms.responses.list({ formId });
+    console.log(responses)
+    return responses;
 }
 //example usage is getFormResponses(forms,'1KKE9CKONUECsCMMTLWql0Q9PPmgk7z2RH0FuQ6-rLq0')
-
+const responses = getFormResponses(auth,'1KKE9CKONUECsCMMTLWql0Q9PPmgk7z2RH0FuQ6-rLq0')
