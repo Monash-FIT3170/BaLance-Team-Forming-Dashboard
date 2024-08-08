8652c850a7022ec61298fa80aab9763bf85dd1a0
Baaset Moslih <s.a.baaset.moslih@gmail.com>
2024-08-09 00:36:34 +1000

    fix: minor fixed to generator.py and generation of mock data

--- docs/mockInputs/generator.py
+++ docs/mockInputs/generator.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 from datetime import datetime, timedelta
 from random import randint
-from uuid import uuid4
 import pandas as pd
 import requests, argparse
 import pprint as p
 import datetime
-import csv
 
 ###########################################
 ###    command line input validation    ###
@@ -64,25 +62,25 @@ endpoint_body = [
     }
 ]
 
-# response = requests.post(mockaroo_endpoint, params=endpoint_params, json=endpoint_body)
+response = requests.post(mockaroo_endpoint, params=endpoint_params, json=endpoint_body)
 
-# if not response.ok:
-#     raise Exception("Call to Mockaroo API returned response NOT ok")
+if not response.ok:
+    raise Exception("Call to Mockaroo API returned response NOT ok")
 
-response = [{'lastName': 'Wormleighton', 'nameFemale': 'Evanne', 'nameMale': 'Sutherland'}, {'lastName': 'Bromby', 'nameFemale': 'Stormie', 'nameMale': 'Whitman'}, {'lastName': 'Bolliver', 'nameFemale': 'Wally', 'nameMale': 'Clarence'}, {'lastName': 'Marling', 'nameFemale': 'Marylee', 'nameMale': 'Randal'}, {'lastName': 'Whitnall', 'nameFemale': 'Dacia', 'nameMale': 'Valdemar'}, {'lastName': 'Bonde', 'nameFemale': 'Michal', 'nameMale': 'Welsh'}, {'lastName': 'Huckle', 'nameFemale': 'Hildy', 'nameMale': 'Land'}, {'lastName': 'Tarbert', 'nameFemale': 'Tracy', 'nameMale': 'Coleman'}, {'lastName': 'Cocklin', 'nameFemale': 'Beret', 'nameMale': 'Chilton'}, {'lastName': 'Cammidge', 'nameFemale': 'Lucine', 'nameMale': 'Basilius'}, {'lastName': 'McFayden', 'nameFemale': 'Sharona', 'nameMale': 'Olivier'}, {'lastName': 'Bente', 'nameFemale': 'Zita', 'nameMale': 'Garner'}, {'lastName': 'Rawstorne', 'nameFemale': 'Krista', 'nameMale': 'Flory'}, {'lastName': 'Robotham', 'nameFemale': 'Merna', 'nameMale': 'Hermy'}, {'lastName': 'Furmenger', 'nameFemale': 'Clemmie', 'nameMale': 'Kain'}, {'lastName': 'Maffin', 'nameFemale': 'Sissie', 'nameMale': 'Mackenzie'}, {'lastName': 'Crinion', 'nameFemale': 'Edee', 'nameMale': 'Arnie'}, {'lastName': 'Howen', 'nameFemale': 'Anna-diana', 'nameMale': 'Enoch'}, {'lastName': 'Peasee', 'nameFemale': 'Reina', 'nameMale': 'Hill'}, {'lastName': 'Szymonowicz', 'nameFemale': 'Orella', 'nameMale': 'Drew'}, {'lastName': 'Batalini', 'nameFemale': 'Dasie', 'nameMale': 'Lion'}, {'lastName': 'Matushevitz', 'nameFemale': 'Phyllys', 'nameMale': 'Kenon'}, {'lastName': 'Cromer', 'nameFemale': 'Chantal', 'nameMale': 'Germaine'}, {'lastName': 'Nairne', 'nameFemale': 'Julietta', 'nameMale': 'Rockwell'}, {'lastName': 'Money', 'nameFemale': 'Kassie', 'nameMale': 'Rocky'}, {'lastName': 'Spight', 'nameFemale': 'Nedi', 'nameMale': 'Glynn'}, {'lastName': "M'Quharge", 'nameFemale': 'Lurline', 'nameMale': 'Gran'}, {'lastName': 'Elsop', 'nameFemale': 'Fenelia', 'nameMale': 'Delainey'}, {'lastName': 'Pawlik', 'nameFemale': 'Joeann', 'nameMale': 'Gard'}, {'lastName': 'Challice', 'nameFemale': 'Adelheid', 'nameMale': 'Spenser'}, {'lastName': 'Glamart', 'nameFemale': 'Jennilee', 'nameMale': 'Penrod'}, {'lastName': 'Watterson', 'nameFemale': 'Emmaline', 'nameMale': 'Jasen'}, {'lastName': 'Wastling', 'nameFemale': 'Peria', 'nameMale': 'Maxim'}, {'lastName': 'Waycot', 'nameFemale': 'Georgianne', 'nameMale': 'Weidar'}, {'lastName': 'Wolfarth', 'nameFemale': 'Kaja', 'nameMale': 'Shepperd'}, {'lastName': 'Janikowski', 'nameFemale': 'Rachael', 'nameMale': 'Pascale'}, {'lastName': 'Burston', 'nameFemale': 'Rozalie', 'nameMale': 'Enrique'}, {'lastName': 'Lye', 'nameFemale': 'Sidoney', 'nameMale': 'Rock'}, {'lastName': 'Gaynor', 'nameFemale': 'Claudelle', 'nameMale': 'Dolf'}, {'lastName': 'Mollnar', 'nameFemale': 'Rosanna', 'nameMale': 'Randie'}, {'lastName': 'Scamel', 'nameFemale': 'Delcina', 'nameMale': 'Gaspard'}, {'lastName': 'Tettersell', 'nameFemale': 'Simone', 'nameMale': 'Alex'}, {'lastName': 'Nerval', 'nameFemale': 'Tessie', 'nameMale': 'Pernell'}, {'lastName': 'Spaule', 'nameFemale': 'Teddy', 'nameMale': 'Herb'}, {'lastName': 'Pickthorn', 'nameFemale': 'Alla', 'nameMale': 'Aldon'}, {'lastName': "D'eath", 'nameFemale': 'Madelina', 'nameMale': 'Ricky'}, {'lastName': 'Jedrys', 'nameFemale': 'Wrennie', 'nameMale': 'Harley'}, {'lastName': 'Crennan', 'nameFemale': 'Harri', 'nameMale': 'Bowie'}, {'lastName': 'Miliffe', 'nameFemale': 'Sibeal', 'nameMale': 'Benedict'}, {'lastName': 'Gubbins', 'nameFemale': 'Florencia', 'nameMale': 'Haze'}]
+mock_names = response.json()
 
 # determine ratio of female to male
 num_males = round(float(randint(0, 100))/100 * args.rows)
 
 students = [{
-        'lastName': response[i]['lastName'],
-        'preferredName': response[i]['nameMale'],
+        'lastName': mock_names[i]['lastName'],
+        'preferredName': mock_names[i]['nameMale'],
         'gender': 'M'
     } for i in range(0, num_males)] + [{
-        'lastName': response[i]['lastName'],
-        'preferredName': response[i]['nameFemale'],
+        'lastName': mock_names[i]['lastName'],
+        'preferredName': mock_names[i]['nameFemale'],
         'gender': 'F'
-    } for i in range(num_males, len(response))]
+    } for i in range(num_males, len(mock_names))]
 
 id_counter = 10000001
 lab_counter = 1
@@ -173,13 +171,11 @@ belbin_df.to_csv("belbin.csv", header=True, index=False, lineterminator="\n")
 effort_df.to_csv("effort.csv", header=True, index=False, lineterminator="\n")
 time_pref_df.to_csv("time_pref.csv", header=True, index=False, lineterminator="\n")
 
-
 for file_name in ["students.csv", "belbin.csv", "effort.csv", "time_pref.csv"]:
-    with open(file_name, 'r') as file:
-        lines = list(csv.reader(file))
-
-    lines = lines[:-1]
-
-    with open(file_name, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(lines)
+    # obtained from https://community.alteryx.com/t5/Alteryx-Designer-Desktop-Discussions/Blank-Line-at-the-end-of-csv-file/td-p/1256551
+    with open(file_name) as objFile:
+        lines = objFile.readlines()
+        last_line = lines[len(lines)-1]
+        lines[len(lines)-1] = last_line.rstrip()
+    with open(file_name, 'w') as objFile:    
+        objFile.writelines(lines)
