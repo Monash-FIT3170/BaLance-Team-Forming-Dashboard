aae116b7b5eea99b4c436e8322ce93ef9b906091
Ethan Chuen <ethan.chuen@gmail.com>
2024-08-13 22:05:52 +1000

    update create form modal
    - add modal buttons and checklist

--- frontend/src/components/homePage/CreateFormModal.jsx
+++ frontend/src/components/homePage/CreateFormModal.jsx
@@ -1,4 +1,5 @@
 import {
+  Checkbox,
   Flex,
   FormControl,
   FormLabel,
@@ -7,25 +8,76 @@ import {
   ModalCloseButton,
   ModalContent,
   ModalHeader,
-  ModalOverlay,
+  ModalOverlay, Stack,
   Text,
   useToast,
   VStack,
 } from "@chakra-ui/react";
+import {useState} from "react";
+import ModalFooterButtonPair from "../_shared/ModalFooterButtonPair.jsx";
 
 const CreateFormModal = ({ isModalOpen, onModalClose }) => {
+  const [submitted, setSubmitted] = useState(false);
 
   const closeModal = () => {
+    setSubmitted(false)
     onModalClose();
   };
 
+  const sendForm = (event) => {
+    setSubmitted(true);
+  }
+
+  const renderForm = () => {
+    return (
+      <form id="create-google-form" onSubmit={sendForm}>
+        <br />
+        <FormControl isRequired>
+          <Stack spacing={5} direction='column'>
+            <Checkbox>Belbin</Checkbox>
+            <Checkbox>Preference</Checkbox>
+          </Stack>
+        </FormControl>
+      </form>
+    )
+  }
+
   return (
-    <Modal isOpen={isModalOpen} onClose={closeModal}>
+    <Modal closeOnOverlayClick={false} isOpen={isModalOpen} onClose={closeModal}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>New Form</ModalHeader>
         <ModalCloseButton />
         <hr />
+
+        <ModalBody pb={10}>
+          {!submitted ? (
+            renderForm()
+          ) :(
+            <Text>Sample text</Text>
+          )}
+        </ModalBody>
+
+        {!submitted ? (
+          <ModalFooterButtonPair
+            cancelButtonColor="red"
+            cancelButtonOnClick={closeModal}
+            cancelButtonText="Cancel"
+            confirmButtonColor="blue"
+            confirmButtonText="Send Form"
+          />
+        ) : (
+          <ModalFooterButtonPair
+            cancelButtonColor="red"
+            cancelButtonOnClick={() => {
+              closeModal();
+              window.location.reload();
+            }}
+            cancelButtonText="Later"
+            confirmButtonColor="blue"
+            confirmButtonText="Confirm"
+          />
+        )}
 
       </ModalContent>
     </Modal>
