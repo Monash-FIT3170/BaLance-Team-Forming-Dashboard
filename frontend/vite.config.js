c54c4aef822f461e92296f5cd3596f26e33eeaed
Alucardigan <thejas.t.vinod@gmail.com>
2024-09-13 22:02:29 +1000

    TEST: frontend environment changes for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-console.log('Backend Target:', process.env.BACKEND_TARGET);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: "0.0.0.0",
-        port: 3000,
-        proxy: {
-            "/api": {
-                target: process.env.BACKEND_TARGET,
-                changeOrigin: true
+export default ({ mode }) => {
+    // Load environment variables based on the current mode (development, production, etc.)
+    const env = loadEnv(mode, process.cwd());
+
+    console.log('Backend Target:', env.VITE_BACKEND_URL);
+
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            host: "0.0.0.0",
+            port: 3000,
+            proxy: {
+                "/api": {
+                    target: env.VITE_BACKEND_URL,
+                    changeOrigin: true
+                }
             }
         }
-    }
-})
+    });
+};
