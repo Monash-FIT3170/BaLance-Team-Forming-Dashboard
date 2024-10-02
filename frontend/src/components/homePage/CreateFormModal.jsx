7e022f236d80c31cde955e676a5bd69c4e3008f1
Trevor <tyao0006l@student.monash.edu>
2024-10-02 18:44:18 +1000

    create checkbox options for belbin, timepref, effort. Initial pseudocode for form creation

--- frontend/src/components/homePage/CreateFormModal.jsx
+++ frontend/src/components/homePage/CreateFormModal.jsx
@@ -25,18 +25,68 @@ const CreateFormModal = ({ isModalOpen, onModalClose }) => {
     onModalClose();
   };
 
+  //Checkbox states
+  const [formOptions, setFormOptions] = useState({
+    Belbin: false,
+    Effot: false,
+    TimeAndPref: false
+  });
+
+  //Changes checkbox state for form creation
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setFormOptions((prev) => ({
+      ...prev,
+      [name]: checked,
+    }));
+  };
+
   const sendForm = (event) => {
+    const forms = google.forms({version:'v1', auth: jwclient});
+
+    //Create google form based on checkbox options
+    const form = {
+
+    //Belbin questions
+
+    //Effort questions
+
+    //Time preference questions
+    }
+    
+    //Get students from unit
     event.preventDefault();
     setSubmitted(true);
+
+
+    //Backend api call
   };
 
   const renderForm = () => (
     <form id="create-google-form" onSubmit={sendForm}>
       <FormControl isRequired>
         <Stack spacing={5} direction='column'>
-          <Checkbox>Belbin</Checkbox>
-          <Checkbox>Effort</Checkbox>
-          <Checkbox>Time & Preference</Checkbox>
+          <Checkbox
+          name="Belbin"
+          isChecked={formOptions.Belbin}
+          onChange={handleCheckboxChange}>
+          
+            Belbin
+          </Checkbox>
+
+          <Checkbox
+           name="Effort"
+           isChecked={formOptions.Effort}
+           onChange={handleCheckboxChange}>
+            Effort
+          </Checkbox>
+
+          <Checkbox
+          name="TimeAndPreference"
+          isChecked={formOptions.TimeAndPreference}
+          onChange={handleCheckboxChange}>
+            Time & Preference
+          </Checkbox>
         </Stack>
       </FormControl>
       <Box position="relative">
