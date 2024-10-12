d1174cb5077b5cdff2c0ac85436fec20749b4c27
Lachlan Williams <lwil0039@student.monash.edu>
2024-10-12 14:52:38 +1100

    Documentation: auth0Middleware

--- backend/middleware/auth0Middleware.js
+++ backend/middleware/auth0Middleware.js
@@ -1,7 +1,7 @@
 const { auth } = require('express-oauth2-jwt-bearer');
 const axios = require('axios');
 
-
+// Function to retrieve user details from Auth0 using the access token in the request
 async function getUserDetails(req) {
     const accessToken = req.auth.token;
         const userResponse = await axios.get(process.env.AUTH_DOMAIN+ 'userinfo',
@@ -14,12 +14,13 @@ async function getUserDetails(req) {
         return userResponse.data;
 }
 
+// Function to delay execution for a specified amount of time
 async function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
 
-
+// Middleware function to handle JWT verification and user info retrieval for Auth0
 const auth0Middleware = (app) => {
     const verifyJwt = auth({
         audience: 'balance-api-endpoint',
@@ -29,6 +30,7 @@ const auth0Middleware = (app) => {
     
     app.use(verifyJwt);
 
+    // Middleware to retrieve user details and handle retries on failure
     app.use(async (req, res, next) => {
         try {
             req.user = await getUserDetails(req);
